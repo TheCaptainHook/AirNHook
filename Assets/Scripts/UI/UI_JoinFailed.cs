4ca5142211f1fe6f0c25cb4ec312af0eecbbebd2
Orknology <kjunha11@gmail.com>
2024-03-12 15:14:01 +0900

    Feat: 연결 실패 UI, 포트번호 변경

--- Assets/Scripts/UI/UI_JoinFailed.cs
+++ Assets/Scripts/UI/UI_JoinFailed.cs
@@ -1,32 +1,25 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
-using Mirror;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Join : UI_Base
+public class UI_JoinFailed : UI_Base
 {
     [SerializeField] private GameObject _mainFrame;
 
-    [SerializeField] private TMP_InputField _inputField;
-
-    [SerializeField] private Button _joinBtn;
+    [SerializeField] private Button _retryBtn;
     [SerializeField] private Button _exitBtn;
-
-    private void Start()
-    {
-        _inputField.text = Managers.Network.networkAddress;
-        _joinBtn.onClick.AddListener(OnJoinBtn);
-        _exitBtn.onClick.AddListener(OnExitBtn);
-    }
-
     public override void OnEnable()
     {
         OpenUI();
         Show();
+    }
+    
+    private void Start()
+    {
+        _retryBtn.onClick.AddListener(OnRetryBtn);
+        _exitBtn.onClick.AddListener(OnExitBtn);
     }
     
     private void Show()
@@ -36,13 +29,10 @@ public class UI_Join : UI_Base
         seq.Append(_mainFrame.transform.DOScale(1.1f, 0.2f));
         seq.Append(_mainFrame.transform.DOScale(1f, 0.1f));
     }
-
-    private void OnJoinBtn()
+    
+    private void OnRetryBtn()
     {
-        var ipString = _inputField.text;
-        Managers.Network.networkAddress = ipString;
-        Managers.Network.StartClient();
-
+        Managers.UI.ShowUI<UI_Join>();
         _mainFrame.transform.localScale = Vector3.one * 0.1f;
         CloseUI();
     }
@@ -53,4 +43,6 @@ public class UI_Join : UI_Base
         _mainFrame.transform.localScale = Vector3.one * 0.1f;
         CloseUI();
     }
+    
+    
 }
