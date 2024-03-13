b1213cc9176d18ef6783f7834dbf84ee17ab594d
pure-night <58593218+pure-night@users.noreply.github.com>
2024-03-13 15:12:33 +0900

    Feat: Language UI dropdown setting

--- Assets/Scripts/UI/UI_Language.cs
+++ Assets/Scripts/UI/UI_Language.cs
@@ -13,11 +13,10 @@ public class UI_Language : UI_Base
     private Dictionary<string, string> _dict;
     private List<string> _fileNames;
     private List<string> _languageNames;
+    private int _dropDownIndex;
 
     protected override void Start()
     {
-        base.Start();
-        
         GetLanguageList();
     }
 
@@ -33,10 +32,18 @@ public class UI_Language : UI_Base
 
         _fileNames = Managers.Data.GetJsonNames(path, "*.json");
         
-        _languageNames = _fileNames.Select(fileName =>
-                JsonConvert.DeserializeObject<Sentence[]>(File.ReadAllText(path + fileName + ".json")))
-            .Select(list => list[0].text).ToList();
-
+        var currentLanguage = PlayerPrefs.GetString("Language");
+        _languageNames = new List<string>();
+        for (var i = 0; i < _fileNames.Count; i++)
+        {
+            var sentences = JsonConvert.DeserializeObject<Sentence[]>(File.ReadAllText(path + _fileNames[i] + ".json"));
+            
+            _dropDownIndex = _fileNames[i] == currentLanguage ? i : 0;
+            
+            if (sentences.Length > 0)
+                _languageNames.Add(sentences[0].text);
+        }
+        
         _dict = new Dictionary<string, string>();
         for (var i = 0; i < _fileNames.Count; i++)
         {
@@ -45,6 +52,7 @@ public class UI_Language : UI_Base
         
         _languageDropDown.ClearOptions();
         _languageDropDown.AddOptions(_languageNames);
+        _languageDropDown.value = _dropDownIndex;
     }
 
     public void SetLocalization(int index)
