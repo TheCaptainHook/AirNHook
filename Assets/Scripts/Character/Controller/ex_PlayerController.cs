20daa7ad1d42a5f0b8228a379c184945046f2e9c
PHT97 <park9704271@naver.com>
2024-03-13 16:25:56 +0900

    <feat> 2번째방법 실패

--- Assets/Scripts/Character/Controller/ex_PlayerController.cs
+++ Assets/Scripts/Character/Controller/ex_PlayerController.cs
@@ -3,17 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class ex_PlayerController : MonoBehaviour
 {
     public enum eState
     {
-        IDLE,MOVE,DIE
+        IDLE,MOVE,DIE,JUMP
     }
 
     private eState _state = eState.IDLE;//플레이어의 현재상태
     private bool _isDie; //사망여부확인
     private Vector2 _moveDir;
+    private double _holdingTime;
+    private Rigidbody2D _rigidbody;
+    private Vector2 jumpForce;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     private void Start()
     {
@@ -45,9 +54,39 @@ public class ex_PlayerController : MonoBehaviour
             }
         }
     }
-    private void OnMove(InputValue value)
+    public void OnMove(InputAction.CallbackContext ctx)
     {
-        Vector2 dir = value.Get<Vector2>();
+        Vector2 dir = ctx.ReadValue<Vector2>();
         this._moveDir = dir;
+
+        if(ctx.phase == InputActionPhase.Canceled)
+        {
+            this._state = eState.IDLE;
+        }
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Jump");
+        if(ctx.interaction is HoldInteraction)
+        {//holding 중
+            if(ctx.duration > 0.5 && ctx.duration < 1.5)
+            {
+                this._holdingTime = ctx.duration;
+            }
+            else if(ctx.duration > 1.5)
+            {
+                this._holdingTime = 1.5;
+            }
+            else
+            {
+                this._holdingTime = 0.8;
+            }
+        }
+        else if(ctx.interaction is PressInteraction)
+        {//press했을 경우
+            this._rigidbody.AddForce(Vector2.up * this.jumpForce * (float)this._holdingTime, ForceMode2D.Impulse);
+            this._state = eState.JUMP;
+        }
     }
 }
