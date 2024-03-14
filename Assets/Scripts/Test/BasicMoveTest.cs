8b0a712a8d4c25ec2c54fc44c15d8c7613833cfa
pure-night <58593218+pure-night@users.noreply.github.com>
2024-03-14 10:58:38 +0900

    Feat: Instantiate Emote For Network

--- Assets/Scripts/Test/BasicMoveTest.cs
+++ Assets/Scripts/Test/BasicMoveTest.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BasicMoveTest : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         var randPos = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
         transform.position += randPos;
+    }
+
+    [Command]
+    public void CmdEmote(string emoteName)
+    {
+        var prefab = Managers.Network.spawnPrefabDict[emoteName];
+        var go = Instantiate(prefab, gameObject.transform.position, Quaternion.identity);
+        NetworkServer.Spawn(go);
+        go.name = prefab.name;
+        RpcEmote(go);
+    }
+
+    [ClientRpc]
+    public void RpcEmote(GameObject go)
+    {
+        var sort = go.GetComponent<SortingGroup>();
+        sort.sortingOrder = isLocalPlayer ? 8 : 7;
+        go.transform.parent = gameObject.transform;
     }
 }
