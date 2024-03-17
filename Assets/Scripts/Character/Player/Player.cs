77cc9045262422a5213e672a6529bd788b325fe4
PHT97 <park9704271@naver.com>
2024-03-17 14:08:59 +0900

    Feat: 점프버퍼제작

--- Assets/Scripts/Character/Player/Player.cs
+++ Assets/Scripts/Character/Player/Player.cs
@@ -7,8 +7,8 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     //TODO 점프 버퍼
-    //[SerializeField] private float _jumpBufferTime = 1f;
-    //private float _jumpBufferCount;
+    [SerializeField] private float _jumpBufferTime = 0.2f;
+    private float _jumpBufferCount;
 
     //코요테시간
     [SerializeField] private float _coyoteTime = 0.2f;
@@ -25,6 +25,9 @@ public class Player : MonoBehaviour
     private float _speed = 4f;
     private float _jumpingPower = 20f;
 
+    //점프상태 체크
+    private bool _isJumping = true;
+
     private void Awake()
     {
         _rigidbd = GetComponent<Rigidbody2D>();
@@ -39,6 +42,20 @@ public class Player : MonoBehaviour
         else
         {
             _coyoteTimeCount -= Time.deltaTime;
+        }
+
+        //점프를 눌렀을 때
+        if (Input.GetButtonDown("Jump")){}
+        //점프를 누르지 않았을 때
+        else
+        {
+            _jumpBufferCount -= Time.deltaTime;
+        }
+        //
+        if (_coyoteTimeCount > 0f && _jumpBufferCount > 0f)
+        {
+            _rigidbd.velocity = new Vector2(_rigidbd.velocity.x, _jumpingPower);
+            _jumpBufferCount = 0f;
         }
     }
 
@@ -67,7 +84,7 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbd.velocity = new Vector2(_horizontal * _speed, _rigidbd.velocity.y);
-        //IsHead();
+        IsHead();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -75,31 +92,25 @@ public class Player : MonoBehaviour
         _horizontal = context.ReadValue<Vector2>().x;
     }
 
+    
     public void JumpStarted(InputAction.CallbackContext context)
     {
-        //context.phase == InputActionPhase.Started /  context.started && _coyoteTimeCount > 0f
-        if (_coyoteTimeCount > 0f)
-        {
-            _rigidbd.velocity = new Vector2(_rigidbd.velocity.x, _jumpingPower);
-        }
+        _jumpBufferCount = _jumpBufferTime;
     }
 
     public void JumpCanceled(InputAction.CallbackContext context)
     {
-        if (_rigidbd.velocity.y > 0f)
+        if (_rigidbd.velocity.y >= 0f)
         {
             _rigidbd.velocity = new Vector2(_rigidbd.velocity.x, _rigidbd.velocity.y * 0.6f);
-
             _coyoteTimeCount = 0f;
         }
     }
+    
 
+    //점프체크
     private bool IsFloor()
     {
-        // OverlapCircle <- 매개변수로 전달할 위치를 기준으로 반지름만큼 원 생성
-        //그 영역 내에 충돌체를 가진 게임오브젝트가 있는지 검사
-        //return Physics2D.OverlapCircle(_floorCheck.position <- 캐릭터 바닥, 0.45f, _floorLayer);
-        
         //Ray발사
         for (int i = -1; i < 2; i++)
         {
@@ -111,39 +122,19 @@ public class Player : MonoBehaviour
         return false;
     }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawRay(transform.position + (transform.right * 0.5f) + (Vector3.up * 0.01f), Vector2.down * 0.1f);
-        Gizmos.DrawRay(transform.position + (-transform.right * 0.5f) + (Vector3.up * 0.01f), Vector2.down * 0.1f);
-    }
-
+    //머리 충돌처리 정상작동
+    //이제 부드럽게 밀려나서 점프되도록 제작
     private bool IsHead()
     {
-        var rays = new Ray[4];
+        //레이 발사
+        for(int i = -2; i < 3; i++)
         {
-            new Ray(transform.position + (transform.right * 0.2f) + (Vector3.up * 1f), Vector2.up);
-            new Ray(transform.position + (-transform.right * 0.2f) + (Vector3.up * 1f), Vector2.up);
-            new Ray(transform.position + (transform.right * 0.5f) + (Vector3.up * 1f), Vector2.up);
-            new Ray(transform.position + (-transform.right * 0.5f) + (Vector3.up * 1f), Vector2.up);
-        }
-        for(int i = 0; i < rays.Length; i++)
-        {
-            if(Physics.Raycast(rays[i], 0.2f, _floorLayer))
+            if(Physics2D.Raycast(transform.position + (Vector3.right * 0.25f * i), Vector2.up, 1.1f, _floorLayer))
             {
-                Debug.Log("!@");
+                Debug.Log("Head Hit!!");
                 return true;
             }
         }
         return false;
     }
-    //Ray를 보여주는 코드
-    //private void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawRay(transform.position + (transform.right * 0.2f) + (Vector3.up * 1f), Vector2.up * 0.1f);
-    //    Gizmos.DrawRay(transform.position + (-transform.right * 0.2f) + (Vector3.up * 1f), Vector2.up * 0.1f);
-    //    Gizmos.DrawRay(transform.position + (transform.right * 0.5f) + (Vector3.up * 1f), Vector2.up * 0.1f);
-    //    Gizmos.DrawRay(transform.position + (-transform.right * 0.5f) + (Vector3.up * 1f), Vector2.up * 0.1f);
-    //}
 }
