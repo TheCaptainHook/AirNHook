b584ed15c445e93c16e094be98246d9a6d2aaaf2
Orknology <kjunha11@gmail.com>
2024-03-20 14:24:28 +0900

    Feat: 장애물 대미지 / 캐릭터 사망 부활 추가

--- Assets/Scripts/MapObjects/DamageableObejct.cs
+++ Assets/Scripts/MapObjects/DamageableObejct.cs
@@ -1,17 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageableObject : MonoBehaviour
+public class DamageableObejct : MonoBehaviour
 {
+    [SerializeField] Collider2D _collider2D;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         // 충돌한 객체가 IDamageable 인터페이스를 가지고 있는지 확인
-        IDamageable damageable = other.GetComponent<IDamageable>();
-        if (damageable != null)
+        if (other.TryGetComponent(out IDamageable damageable))
         {
-            // 대미지를 입힘
-            damageable.Die();
+            // If successful, apply damage
+            damageable.TakeDamage();
         }
     }
 }
