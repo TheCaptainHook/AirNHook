371dc01ba95cceefbeffc10e2005afb351938abc
pure-night <58593218+pure-night@users.noreply.github.com>
2024-03-29 19:18:49 +0900

    Feat: block steam relay
    빌드 시에만 Managers에서 block풀기.

--- Assets/Scripts/Manager/Managers.cs
+++ Assets/Scripts/Manager/Managers.cs
@@ -64,11 +64,11 @@ public class Managers : MonoBehaviour
         
         if (networkManager == null)
         {
-#if UNITY_EDITOR
+//#if UNITY_EDITOR
             var go = ResourceManager.Instantiate("Prefabs/Manager/NetworkManagerKCP");
-#else
-            var go = ResourceManager.Instantiate("Prefabs/Manager/NetworkManager");
-#endif
+//#else
+//            var go = ResourceManager.Instantiate("Prefabs/Manager/NetworkManager");
+//#endif
             _instance._network = go.GetComponent<CustomNetworkManager>();
         }
         else
