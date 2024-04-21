89c1904ac1ed95d44dd6c6733238442ae87a3121
PHT97 <park9704271@naver.com>
2024-04-21 11:54:02 +0900

    Feat: PlayData, StageData 병합

--- Assets/Scripts/Map/Buildd/ObjectBuild/ExitPointObj.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ExitPointObj.cs
@@ -113,9 +113,9 @@ public class ExitPointObj : BuildBase
     private void UpdateStageClearData(string mapId)
     {
         if (mapId == "Lobby") return;
-        if (!Managers.Data.loadData.stageData[mapId].stageClear)
+        if (!Managers.Data.loadData.playData[mapId].stageClear)
         {
-            Managers.Data.loadData.stageData[mapId].stageClear = true;
+            Managers.Data.loadData.playData[mapId].stageClear = true;
         }
 
         
--- Assets/Scripts/PlayerData/PlayData.cs
+++ Assets/Scripts/PlayerData/PlayData.cs
@@ -7,9 +7,11 @@ public class PlayData
 {
     //맵 클리어 시간, 최소 데스 수, 스킵 횟수(아 이맵하기싫어 넘겨), 퍼펙트 클리어
     public string stageID;
+    public bool stageClear;
     public float clearTime;
+    public int totalDeath;
     public int deathCount;
     public bool skip;
-    public bool perfectClear;
-    public string perfect;
+    public int stageLevel;
+    public int stageHiddenObject;
 }
