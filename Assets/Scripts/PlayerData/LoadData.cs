b81d2f4d1453fdbd3803ae7a8f86afd35159c06c
PHT97 <park9704271@naver.com>
2024-04-22 00:35:38 +0900

    Feat: 저장위치 변경

--- Assets/Scripts/PlayerData/LoadData.cs
+++ Assets/Scripts/PlayerData/LoadData.cs
@@ -1,22 +1,20 @@
 using Newtonsoft.Json;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LoadData
 {
     public Dictionary<string, PlayData> playData = new Dictionary<string, PlayData>();
     //스테이지 클리어 레벨을 딕셔너리로쓰는 stageClearLevelData
     public Dictionary<int, List<string>> stageClearLevelData = new Dictionary<int, List<string>>();
+
     private int _stagelevel;
 
     public void Setup()
     {
         //TODO 아래부분을 Start가 아닌 다른부분에 넣어서 사용하면됩니다.
-        var playDataPath = Path.Combine(Application.streamingAssetsPath, "PlayDatas/PlayDatas.json");
+        var playDataPath = Application.dataPath + "/PlayData.json";
 
         //playData있는지 없는지 체크
         if (File.Exists(playDataPath))
@@ -33,7 +31,6 @@ public class LoadData
             }
             //Managers.Game.stageLevel = _stagelevel;
             //이곳에서 현재 스테이지레벨을 알려줘야함
-
         }
         else
         {
@@ -43,7 +40,7 @@ public class LoadData
 
     public void DataAdd()
     {
-        var playDataPath = Path.Combine(Application.streamingAssetsPath, "PlayDatas/PlayDatas.json");
+        var playDataPath = Application.dataPath + "/PlayData.json";
         //playData,stageData에 데이터넣기
         foreach (var key in Managers.Data.mapData.mapMainDictionary.Keys)
         {
@@ -72,13 +69,12 @@ public class LoadData
         }
         //해당위치에 해당파일이 있는지 체크하고 없으면 생성
         File.WriteAllText(playDataPath, JsonConvert.SerializeObject(playData.Values, Formatting.Indented));
-
     }
 
     //TODO 스테이지 클리어가되면 동시에 작업이 이루어져야함
     public void Save()
     {
-        var playDataPath = Path.Combine(Application.streamingAssetsPath, "PlayDatas/PlayDatas.json");
+        var playDataPath = Application.dataPath + "/PlayData.json";
 
         File.WriteAllText(playDataPath, JsonConvert.SerializeObject(playData.Values, Formatting.Indented));
     }
