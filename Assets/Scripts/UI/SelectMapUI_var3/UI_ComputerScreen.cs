55a4097570f1e2a0731ad6b8ac453dc61cc78f56
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-11-22 16:32:29 +0900

    feat/Audio,RopeTile,....
    Map
    - AudioType -> AudioName
    MapEditor,Util
    -Modification of logic related to audio
    SoundManager
    -Dic key, audioClip.ToString() -> audioClip.Name

--- Assets/Scripts/UI/SelectMapUI_var3/UI_ComputerScreen.cs
+++ Assets/Scripts/UI/SelectMapUI_var3/UI_ComputerScreen.cs
@@ -27,7 +27,7 @@ public class UI_ComputerScreen : UI_Base
 
     public override void OnEnable()
     {
-        transform.localPosition = new Vector3(12, 0.5f);
+        transform.localPosition = new Vector3(20, 0.5f);
     }
 
     private void Awake()
