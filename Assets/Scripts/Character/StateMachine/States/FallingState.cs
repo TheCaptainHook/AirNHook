5a8be162bc74f7ec8d8adc4becb4b1e488b71cd4
pure-night <58593218+pure-night@users.noreply.github.com>
2024-11-30 18:09:41 +0900

    Fix: half platform

--- Assets/Scripts/Character/StateMachine/States/FallingState.cs
+++ Assets/Scripts/Character/StateMachine/States/FallingState.cs
@@ -30,16 +30,14 @@ public class FallingState : BaseState
 
         if (stateMachine.isJumping || stateMachine.isJumpPerformed)
         {
-            if (stateMachine.player.isDownThroughPlatform || stateMachine.vertical < 0)
+            if (stateMachine.vertical < 0 && stateMachine.player.isHalfPlatform)
             {
-                if (stateMachine.player.isHalfPlatform)
-                {
-                    stateMachine.player.DownThroughHalfPlatform();
-                    return;
-                }
+                stateMachine.player.DownThroughHalfPlatform();
+                return;
             }
             else
             {
+                stateMachine.player.isDownThroughPlatform = false;
                 stateMachine.ChangeState(stateMachine.JumpState);
                 return;
             }
