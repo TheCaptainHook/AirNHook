4f333608ce91d3d76ce5e717511a0a51e1f3dc7b
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-12-11 12:56:49 +0900

    feat/

--- Assets/Scripts/UI/UI_EventEchoDialogue.cs
+++ Assets/Scripts/UI/UI_EventEchoDialogue.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
-using UnityEngine.TextCore;
-using System;
-using Unity.VisualScripting;
 using System.Text;
+using System.Collections;
+using System;
+using System.Collections.Generic;
 
 
 public enum Mark
@@ -24,10 +21,14 @@ public class UI_EventEchoDialogue : UI_Base
     [SerializeField] TextMeshProUGUI main_Text;//Main_Text
     [Space(20)]
     [SerializeField] TMP_FontAsset font;
-    private float fontSize;
-    private float spacing; // 빈 공백
+    
+    //1210 Text Effect
+    public List<TextMeshEffectStruct> textMeshEffectStructList;
 
-    [SerializeField] RectTransform splitTextBox;
+    #region Mark_1
+    [Header("Mark_1")]
+    public AnimationCurve scaleCurve;
+    #endregion
 
 
     #region  Components
@@ -35,15 +36,10 @@ public class UI_EventEchoDialogue : UI_Base
     #endregion
 
     #region Dialogue
-    float startX;
-    float currentX;
-    Vector3 center;
-    int index;
+   
 
     //Mark
     Mark mark;
-    public List<EventEchoText> eventEchoTextList;
-    public List<EventEchoText> mark_1List;
 
     #endregion
 
@@ -56,35 +52,34 @@ public class UI_EventEchoDialogue : UI_Base
     #endregion
 
 
-    protected override void Start()
-    {
-        Init();
+    private void Update(){
+        //test
+        if(Input.GetKeyDown(KeyCode.P)){
+            UpdateTextMeshEffectStructList(ref testSentence);
 
-        StartDialogue(testSentence);
+            main_Text.text = testSentence;
+            
 
+            foreach(TextMeshEffectStruct data in textMeshEffectStructList){
+                ChangeColor(data,Color.red);
+            }
+
+            StartCoroutine(StartAnimateTextMesh());
+        }
     }
 
     private void Init()
     {
-        center = splitTextBox.TransformPoint(splitTextBox.rect.center);
-        mark_1List = new();
-        eventEchoTextList = new();
-        fontSize = main_Text.fontSize;
-        spacing = GetSpaceWidth();
+      textMeshEffectStructList = new();
     }
 
     private void Reset()
     {
-        startX = 0;
-        currentX = 0;
-        index = 0;
-        eventEchoTextList.Clear();
-        mark_1List.Clear();
-        mark = Mark.Default;
+     textMeshEffectStructList.Clear();
     }
 
 
-
+    //
 
     #region Main
     private void StartDialogue(string sentence) //0
@@ -94,12 +89,9 @@ public class UI_EventEchoDialogue : UI_Base
 
         //main text.text
         main_Text.text = sentence;
-        Vector2 preferredValues = main_Text.GetPreferredValues(main_Text.text);
-        startX = center.x - preferredValues.x / 2f;
-        currentX = startX;
 
-        CreateTextMesh(sentence.ToCharArray());
 
+        
         //CreateTextMesh("aaaaaa".ToCharArray());
         //CreateTextMesh("bbbbbb".ToCharArray());
         //CreateTextMesh("cccccc".ToCharArray());
@@ -107,60 +99,6 @@ public class UI_EventEchoDialogue : UI_Base
     }
 
 
-
-
-    private void CreateTextMesh(char[] characters)
-    {
-        foreach (char c in characters)
-        {
-            CheckMark(c, ref mark);
-            if (c == '[' || c == ']') continue;
-
-            if (c == ' ') // 공백 처리 (간격만 추가)
-            {
-                currentX += spacing; // 공백은 적당히 조정
-                continue;
-            }
-
-           
-            // 프리팹 생성
-            GameObject charObject = new GameObject("Split");
-            TextMeshProUGUI text = charObject.AddComponent<TextMeshProUGUI>();
-
-            text.text = c.ToString();
-            text.font = font;
-            text.fontSize = fontSize;
-            text.color = main_Text.color;
-            // 위치 지정
-            RectTransform charRect = charObject.GetComponent<RectTransform>();
-            charRect.anchoredPosition = new Vector2(currentX, center.y);
-
-            // 현재 X 위치 업데이트
-            Vector2 charSize = text.GetPreferredValues(text.text);
-            currentX += charSize.x + 0.25f;
-
-            charObject.transform.SetParent(splitTextBox);
-            EventEchoText echotext = new EventEchoText(index, text, c);
-
-            index++;
-
-            switch (mark)
-            {
-                case Mark.Default:
-                    break;
-                case Mark.Mark_1:
-                    mark_1List.Add(echotext);
-                    break;
-                case Mark.Mark_2:
-                    break;
-                case Mark.Mark_3:
-                    break;
-            }
-
-            eventEchoTextList.Add(echotext);
-
-        }
-    }
     #endregion
 
 
@@ -181,10 +119,10 @@ public class UI_EventEchoDialogue : UI_Base
     #endregion
 
     #region  Util
-    private float GetSpaceWidth()
-    {
-        return 0.25f * fontSize;
-    }
+    // private float GetSpaceWidth()
+    // {
+    //     return 0.25f * fontSize;
+    // }
 
     private string GetDialogue(int id) {
         return Managers.Data.language.dict[id];
@@ -216,22 +154,138 @@ public class UI_EventEchoDialogue : UI_Base
         }
     }
     #endregion
-
-
-
-    [Serializable]
-    public struct EventEchoText
+    bool isAnimating;
+    float duration = 1;
+    private IEnumerator StartAnimateTextMesh()
     {
-        public int index;
-        public TextMeshProUGUI text;
-        public char c;
+        isAnimating = true;
+        float elapsed = 0f;
 
-        public EventEchoText(int index, TextMeshProUGUI text,char c)
+        while (elapsed < duration)
         {
-            this.index= index;
-            this.text = text;
-            this.c = c;
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration;
+
+            // AnimationCurve에서 현재 스케일 값 얻기
+            float scaleValue = scaleCurve.Evaluate(t); 
+            UpdateSubstringScale(scaleValue);
+
+            yield return null;
         }
 
+        // 애니메이션 끝난 뒤에는 스케일을 다시 1로 맞춰준다.
+        UpdateSubstringScale(1.0f);
+
+        isAnimating = false;
     }
+
+    // private TMP_TextInfo textInfo;
+    private void UpdateSubstringScale(float scaleValue)
+    {
+        if (string.IsNullOrEmpty(main_Text.text))
+            return;
+
+        TMP_TextInfo textInfo = main_Text.textInfo;
+
+        if (textInfo.characterCount == 0)
+            return;
+
+        // substringRanges 내 모든 범위에 대해 해당 문자들의 스케일 변경
+        for (int r = 0; r < textMeshEffectStructList.Count; r++)
+        {
+            TextMeshEffectStruct data = textMeshEffectStructList[r];
+            int start = data.startIndex;
+            int length = data.textLength;
+
+            for (int i = start; i < start + length && i < textInfo.characterCount; i++)
+            {
+                var charInfo = textInfo.characterInfo[i];
+                if (!charInfo.isVisible)
+                    continue;
+
+                int materialIndex = charInfo.materialReferenceIndex;
+                int vertexIndex = charInfo.vertexIndex;
+
+                Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
+
+                // 문자 중앙점 계산
+                Vector3 charMidBaseline = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2f;
+
+
+
+                // 각 정점을 문자 중심 기준으로 스케일링
+                for (int j = 0; j < 4; j++)
+                {
+                    Vector3 offset = vertices[vertexIndex + j] - charMidBaseline;
+                    vertices[vertexIndex + j] = charMidBaseline + offset * scaleValue;
+                }
+            }
+
+            main_Text.UpdateVertexData(TMP_VertexDataUpdateFlags.Vertices);
+        }
+    }
+    private void ChangeColor(TextMeshEffectStruct data, Color targetColor){
+        main_Text.ForceMeshUpdate();
+
+        for(int i = data.startIndex; i< data.startIndex+data.textLength && i < main_Text.textInfo.characterCount;i++){
+            var charInfo = main_Text.textInfo.characterInfo[i];
+                if (!charInfo.isVisible)
+                    continue;
+
+                int materialIndex = charInfo.materialReferenceIndex;
+                int vertexIndex = charInfo.vertexIndex;
+
+                // vertex 색상 참조
+                Color32[] colors = main_Text.textInfo.meshInfo[materialIndex].colors32;
+
+                colors[vertexIndex + 0] = targetColor;
+                colors[vertexIndex + 1] = targetColor;
+                colors[vertexIndex + 2] = targetColor;
+                colors[vertexIndex + 3] = targetColor;
+        }
+
+        main_Text.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+    }
+
+
+
+    private void UpdateTextMeshEffectStructList(ref string sentence){
+        int startIndex = 0; //1
+        int textLength = 0;
+
+        StringBuilder sb = new();
+
+        //[abcd]e //5
+        for(int i =0; i< sentence.Length;i++){
+            switch(sentence[i]){
+                case '[':
+                    startIndex = i;
+                break;
+                case ']':
+                    textLength = i - startIndex-1;
+                    textMeshEffectStructList.Add(new TextMeshEffectStruct(startIndex,textLength,Mark.Mark_1));
+                break;
+                default:
+                sb.Append(sentence[i]);
+                break;
+            }
+        }
+        sentence = sb.ToString();
+    }
+    
+
+
+[Serializable]
+public struct TextMeshEffectStruct{
+    public int startIndex;
+    public int textLength;
+    public Mark mark;
+    public TextMeshEffectStruct(int startIndex, int textLength,Mark mark){
+        this.startIndex = startIndex;
+        this.textLength =textLength;
+        this.mark = mark;
+    }
+
+}
+
 }
