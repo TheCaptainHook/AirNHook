d444cd17a1cd19979655d79be25983260eec7765
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-12-11 17:56:34 +0900

    feat/ text mesh effect

---
+++ Assets/Editor/UI_EventEchoDialogue_Editor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(UI_EventEchoDialogue))]
+public class UI_EventEchoDialogue_Editor : Editor
+{
+    UI_EventEchoDialogue ui_EventEchoDialogue;
+    Coroutine coroutine;
+    private void OnEnable()
+    {
+        ui_EventEchoDialogue = (UI_EventEchoDialogue)target;
+        coroutine = ui_EventEchoDialogue.mark_1_EffectCoroutine;
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("TEST"))
+        {
+            ui_EventEchoDialogue.Test();
+        }
+
+        if (GUILayout.Button("Reset"))
+        {
+            ui_EventEchoDialogue.Reset();
+        }
+    }
+
+}
--- Assets/Scripts/UI/UI_EventEchoDialogue.cs
+++ Assets/Scripts/UI/UI_EventEchoDialogue.cs
@@ -5,15 +5,17 @@ using System.Text;
 using System.Collections;
 using System;
 using System.Collections.Generic;
+using UnityEditor;
 
 
 public enum Mark
 {
     Default,
-    Mark_1,
-    Mark_2,
+    Mark_1, //[] : Scale
+    Mark_2, //<> : Bounce
     Mark_3
 }
+
 
 public class UI_EventEchoDialogue : UI_Base
 {
@@ -21,22 +23,20 @@ public class UI_EventEchoDialogue : UI_Base
     [SerializeField] TextMeshProUGUI main_Text;//Main_Text
     [Space(20)]
     [SerializeField] TMP_FontAsset font;
-    
+
     //1210 Text Effect
     public List<TextMeshEffectStruct> textMeshEffectStructList;
 
     #region Mark_1
-    [Header("Mark_1")]
-    public AnimationCurve scaleCurve;
-    #endregion
 
+    #endregion
 
     #region  Components
 
     #endregion
 
     #region Dialogue
-   
+
 
     //Mark
     Mark mark;
@@ -44,7 +44,7 @@ public class UI_EventEchoDialogue : UI_Base
     #endregion
 
 
-    string testSentence = "Lorem [Ipsum] is simply dummy /1";
+    public string testSentence = "Lorem [Ipsum] is simply dummy /1";
 
 
     #region Pattern
@@ -52,34 +52,74 @@ public class UI_EventEchoDialogue : UI_Base
     #endregion
 
 
-    private void Update(){
+    private void Update()
+    {
         //test
-        if(Input.GetKeyDown(KeyCode.P)){
-            UpdateTextMeshEffectStructList(ref testSentence);
+        if (Input.GetKeyDown(KeyCode.P))
+        {
 
-            main_Text.text = testSentence;
-            
+            Test();
 
-            foreach(TextMeshEffectStruct data in textMeshEffectStructList){
-                ChangeColor(data,Color.red);
-            }
-
-            StartCoroutine(StartAnimateTextMesh());
         }
     }
 
     private void Init()
     {
-      textMeshEffectStructList = new();
+        textMeshEffectStructList = new();
     }
 
-    private void Reset()
+    public void Reset()
     {
-     textMeshEffectStructList.Clear();
+        textMeshEffectStructList.Clear();
+        StopAllCoroutines();
+        mark_1_EffectCoroutine = null;
+        mark_2_EffectCoroutine = null;
+
+        main_Text.text = "";
+        main_Text.ForceMeshUpdate();
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(main_Text.gameObject); // 텍스트가 속한 게임 오브젝트를 수정된 상태로 표시
+        //UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(UnityEngine.SceneManagement.SceneManager.GetActiveScene()); // 현재 씬을 더티 상태로 표시
+#endif
     }
 
 
-    //
+    public Coroutine mark_1_EffectCoroutine;
+    public Coroutine mark_2_EffectCoroutine;
+
+    public void Test()
+    {
+        string text = testSentence;
+        //1. Replace
+        Replace(ref text);
+        //Search mark
+        UpdateTextMeshEffectStructList(ref text);
+        //main_text init
+        main_Text.text = text;
+
+        main_Text.ForceMeshUpdate();
+        List<TMP_EffectField> fadeInList = new();
+        List<TMP_EffectField> bounceList = new();
+
+        //on Effect
+        foreach (TextMeshEffectStruct data in textMeshEffectStructList)
+        {
+            ChangeColor(data, Color.red);
+
+            if (data.mark == Mark.Mark_1) GetEffectFieldList(data, ref fadeInList);
+            if (data.mark == Mark.Mark_2) GetEffectFieldList(data, ref bounceList);
+        }
+
+        main_Text.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+        if (mark_1_EffectCoroutine != null) StopCoroutine(mark_1_EffectCoroutine);
+        mark_1_EffectCoroutine = StartCoroutine(ScaleEffectCo(fadeInList));
+
+        if (mark_2_EffectCoroutine != null) StopCoroutine(mark_2_EffectCoroutine);
+        mark_2_EffectCoroutine = StartCoroutine(BounceEffectCo(bounceList));
+
+    }
+
 
     #region Main
     private void StartDialogue(string sentence) //0
@@ -91,7 +131,7 @@ public class UI_EventEchoDialogue : UI_Base
         main_Text.text = sentence;
 
 
-        
+
         //CreateTextMesh("aaaaaa".ToCharArray());
         //CreateTextMesh("bbbbbb".ToCharArray());
         //CreateTextMesh("cccccc".ToCharArray());
@@ -124,168 +164,207 @@ public class UI_EventEchoDialogue : UI_Base
     //     return 0.25f * fontSize;
     // }
 
-    private string GetDialogue(int id) {
+    private string GetDialogue(int id)
+    {
         return Managers.Data.language.dict[id];
     }
 
 
-   
+
     private void Replace(ref string sentence)
     {
         StringBuilder sb = new StringBuilder(sentence);
         sb.Replace(stringToIntPattern_PlayerDeath, 5.ToString());
-        
+
         sentence = sb.ToString();
     }
 
-    //onMark_1 : '[]'
-    //onMark_2 : '{}'
-    private void CheckMark(char c, ref Mark mark) //2
+
+    // private TMP_TextInfo textInfo;
+    private void GetEffectFieldList(TextMeshEffectStruct data, ref List<TMP_EffectField> list)
     {
-        if (c == '[')
+        for (int i = data.startIndex; i < data.startIndex + data.textLength; i++)
         {
-            mark = Mark.Mark_1;
+            var charInfo = main_Text.textInfo.characterInfo[i];
+            if (!charInfo.isVisible) continue;
 
-        }
-        if (c == ']')
-        {
-            mark = Mark.Default;
-
+            list.Add(new TMP_EffectField(main_Text.textInfo, charInfo, data.mark));
         }
     }
+
     #endregion
-    bool isAnimating;
-    float duration = 1;
-    private IEnumerator StartAnimateTextMesh()
+
+
+    public float animationSpeed = 2f;      // 애니메이션 속도
+    public float scaleMultiplier = 1.5f;   // 최대 스케일 배수
+    IEnumerator ScaleEffectCo(List<TMP_EffectField> list)
     {
-        isAnimating = true;
-        float elapsed = 0f;
-
-        while (elapsed < duration)
+        float scale;
+        while (true)
         {
-            elapsed += Time.deltaTime;
-            float t = elapsed / duration;
+            scale = 1 + (Mathf.Sin(Time.time * animationSpeed) * (scaleMultiplier - 1f));
 
-            // AnimationCurve에서 현재 스케일 값 얻기
-            float scaleValue = scaleCurve.Evaluate(t); 
-            UpdateSubstringScale(scaleValue);
+            foreach (var f in list)
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    //f.vertices[f.vertexIndex + i] = f.charCenter + (f.vertices[f.vertexIndex + i] - f.charCenter) * scale;
+                    f.vertices[f.vertexIndex + i] = f.charCenter + (f.originalVertices[f.vertexIndex + i] - f.charCenter) * scale;
+                }
+            }
+
+            for (int i = 0; i < main_Text.textInfo.meshInfo.Length; i++)
+            {
+                var meshInfo = main_Text.textInfo.meshInfo[i];
+                meshInfo.mesh.vertices = meshInfo.vertices;
+                main_Text.UpdateGeometry(meshInfo.mesh, i);
+            }
 
             yield return null;
         }
 
-        // 애니메이션 끝난 뒤에는 스케일을 다시 1로 맞춰준다.
-        UpdateSubstringScale(1.0f);
-
-        isAnimating = false;
     }
-
-    // private TMP_TextInfo textInfo;
-    private void UpdateSubstringScale(float scaleValue)
+    float amplitude = 7;
+    IEnumerator BounceEffectCo(List<TMP_EffectField> list)
     {
-        if (string.IsNullOrEmpty(main_Text.text))
-            return;
-
-        TMP_TextInfo textInfo = main_Text.textInfo;
-
-        if (textInfo.characterCount == 0)
-            return;
-
-        // substringRanges 내 모든 범위에 대해 해당 문자들의 스케일 변경
-        for (int r = 0; r < textMeshEffectStructList.Count; r++)
+        float value;
+        while (true)
         {
-            TextMeshEffectStruct data = textMeshEffectStructList[r];
-            int start = data.startIndex;
-            int length = data.textLength;
+            value = (Mathf.Sin(Time.time * 3) + 1) / 2f;
 
-            for (int i = start; i < start + length && i < textInfo.characterCount; i++)
+            foreach (var f in list)
             {
-                var charInfo = textInfo.characterInfo[i];
-                if (!charInfo.isVisible)
-                    continue;
 
-                int materialIndex = charInfo.materialReferenceIndex;
-                int vertexIndex = charInfo.vertexIndex;
-
-                Vector3[] vertices = textInfo.meshInfo[materialIndex].vertices;
-
-                // 문자 중앙점 계산
-                Vector3 charMidBaseline = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2f;
-
-
-
-                // 각 정점을 문자 중심 기준으로 스케일링
-                for (int j = 0; j < 4; j++)
+                for (int i = 0; i < 4; i++)
                 {
-                    Vector3 offset = vertices[vertexIndex + j] - charMidBaseline;
-                    vertices[vertexIndex + j] = charMidBaseline + offset * scaleValue;
+
+                    Vector3 pot = f.charCenter * 4f;
+                    pot.x = 0;
+                    //f.vertices[f.vertexIndex + i] = f.charCenter + (f.vertices[f.vertexIndex + i] - f.charCenter) * scale;
+                    //f.vertices[f.vertexIndex + i] = f.charCenter + (f.originalVertices[f.vertexIndex + i] - f.charCenter) * scale;
+                    f.vertices[f.vertexIndex + i] = f.originalVertices[f.vertexIndex + i] - new Vector3(0, Mathf.Sin(Time.time * animationSpeed) * amplitude, 0);
                 }
             }
 
-            main_Text.UpdateVertexData(TMP_VertexDataUpdateFlags.Vertices);
+            for (int i = 0; i < main_Text.textInfo.meshInfo.Length; i++)
+            {
+                var meshInfo = main_Text.textInfo.meshInfo[i];
+                meshInfo.mesh.vertices = meshInfo.vertices;
+                main_Text.UpdateGeometry(meshInfo.mesh, i);
+            }
+
+            yield return null;
         }
     }
-    private void ChangeColor(TextMeshEffectStruct data, Color targetColor){
-        main_Text.ForceMeshUpdate();
 
-        for(int i = data.startIndex; i< data.startIndex+data.textLength && i < main_Text.textInfo.characterCount;i++){
+
+    private void ChangeColor(TextMeshEffectStruct data, Color targetColor)
+    {
+
+        for (int i = data.startIndex; i < data.startIndex + data.textLength && i < main_Text.textInfo.characterCount; i++)
+        {
             var charInfo = main_Text.textInfo.characterInfo[i];
-                if (!charInfo.isVisible)
-                    continue;
+            if (!charInfo.isVisible)
+                continue;
 
-                int materialIndex = charInfo.materialReferenceIndex;
-                int vertexIndex = charInfo.vertexIndex;
+            int materialIndex = charInfo.materialReferenceIndex;
+            int vertexIndex = charInfo.vertexIndex;
 
-                // vertex 색상 참조
-                Color32[] colors = main_Text.textInfo.meshInfo[materialIndex].colors32;
+            // vertex 색상 참조
+            Color32[] colors = main_Text.textInfo.meshInfo[materialIndex].colors32;
 
-                colors[vertexIndex + 0] = targetColor;
-                colors[vertexIndex + 1] = targetColor;
-                colors[vertexIndex + 2] = targetColor;
-                colors[vertexIndex + 3] = targetColor;
+            colors[vertexIndex + 0] = targetColor / 0.5f;
+            colors[vertexIndex + 1] = targetColor;
+            colors[vertexIndex + 2] = targetColor / 1.5f;
+            colors[vertexIndex + 3] = targetColor / 2;
         }
 
-        main_Text.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
     }
 
 
 
-    private void UpdateTextMeshEffectStructList(ref string sentence){
+    private void UpdateTextMeshEffectStructList(ref string sentence)
+    {
         int startIndex = 0; //1
         int textLength = 0;
-
+        int mark = 0;
         StringBuilder sb = new();
-
-        //[abcd]e //5
-        for(int i =0; i< sentence.Length;i++){
-            switch(sentence[i]){
+        // 0123 45 678 9
+        //[abcd]e [asd]s //5
+        //01234567891123
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            switch (sentence[i])
+            {
                 case '[':
-                    startIndex = i;
-                break;
+                    startIndex = i - mark;
+                    mark++;
+                    break;
                 case ']':
-                    textLength = i - startIndex-1;
-                    textMeshEffectStructList.Add(new TextMeshEffectStruct(startIndex,textLength,Mark.Mark_1));
-                break;
+                    textLength = i - mark - startIndex;
+                    mark++;
+                    textMeshEffectStructList.Add(new TextMeshEffectStruct(startIndex, textLength, Mark.Mark_1));
+                    break;
+                case '<':
+                    startIndex = i - mark;
+                    mark++;
+                    break;
+                case '>':
+                    textLength = i - mark - startIndex;
+                    mark++;
+                    textMeshEffectStructList.Add(new TextMeshEffectStruct(startIndex, textLength, Mark.Mark_2));
+                    break;
                 default:
-                sb.Append(sentence[i]);
-                break;
+                    sb.Append(sentence[i]);
+                    break;
             }
         }
         sentence = sb.ToString();
     }
-    
 
 
-[Serializable]
-public struct TextMeshEffectStruct{
-    public int startIndex;
-    public int textLength;
-    public Mark mark;
-    public TextMeshEffectStruct(int startIndex, int textLength,Mark mark){
-        this.startIndex = startIndex;
-        this.textLength =textLength;
-        this.mark = mark;
+    [Serializable]
+    public struct TextMeshEffectStruct
+    {
+        public int startIndex;
+        public int textLength;
+        public Mark mark;
+        public TextMeshEffectStruct(int startIndex, int textLength, Mark mark)
+        {
+            this.startIndex = startIndex;
+            this.textLength = textLength;
+            this.mark = mark;
+        }
+
     }
 
-}
+
+    public class TMP_EffectField
+    {
+        TMP_TextInfo tmp;
+        public TMP_CharacterInfo charInfo;
+        public Vector3[] vertices;
+        public Vector3[] originalVertices;
+        public int vertexIndex;
+        public int materialIndex;
+        public Vector3 charCenter;
+        public Mark mark;
+
+        public TMP_EffectField(TMP_TextInfo tmp, TMP_CharacterInfo charInfo, Mark mark)
+        {
+            this.tmp = tmp;
+            this.charInfo = charInfo;
+            vertexIndex = charInfo.vertexIndex;
+            materialIndex = charInfo.materialReferenceIndex;
+
+            vertices = tmp.meshInfo[materialIndex].vertices;
+            originalVertices = vertices.Clone() as Vector3[];
+            charCenter = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2;
+
+            this.mark = mark;
+        }
+
+    }
+
 
 }
