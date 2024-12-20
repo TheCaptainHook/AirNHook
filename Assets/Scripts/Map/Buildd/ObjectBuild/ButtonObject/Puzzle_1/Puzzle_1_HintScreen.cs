2854f1141328bdb776082da7b878f577573aa9ea
hygge31 <121877159+hygge31@users.noreply.github.com>
2024-12-20 23:49:29 +0900

    feat/Puzzle_1 hint Screen

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonObject/Puzzle_1/Puzzle_1_HintScreen.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonObject/Puzzle_1/Puzzle_1_HintScreen.cs
@@ -1,4 +1,5 @@
 
+using System.Collections;
 using System.Text;
 using TMPro;
 using UnityEngine;
@@ -7,11 +8,15 @@ public class Puzzle_1_HintScreen : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
 
+    CharInfoField[] charInfos;
+    //float glitchIntensity = 7f; // 흔들리는 강도
+
     public void SetHint(string answer)
     {
         ConvertAnswer(answer);
         //Effect Coroutine
-        
+        Init();
+        StartCoroutine(EffectCo(charInfos));
     }
 
 
@@ -42,6 +47,132 @@ public class Puzzle_1_HintScreen : MonoBehaviour
 
         
     }
-    
+
     #endregion
+
+
+    #region Effect
+ 
+    private void Init()
+    {
+        text.ForceMeshUpdate();
+
+        charInfos = new CharInfoField[text.text.Length];
+        for (int i = 0; i < text.text.Length; i++)
+        {
+            charInfos[i] = new CharInfoField(text.textInfo, text.textInfo.characterInfo[i]);
+        }
+    }
+
+    IEnumerator EffectCo(CharInfoField[] charInfos)
+    {
+        while (true)
+        {
+            int num = Random.Range(0, charInfos.Length);
+            yield return CharEffectCo();
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    IEnumerator CharEffectCo()
+    {
+        float percent = 0;
+
+        char[] cached = text.text.ToCharArray();
+        float changeCharPercent = 0;
+
+        int[] c = new int[2];
+        for (int i = 0; i < 2; i++)
+        {
+            int ran = Random.Range(0, text.text.Length);
+            c[i] = ran;
+        }
+
+        float baseGlitchIntensity = 7f; // 기본 강도
+        float glitchIntensity = baseGlitchIntensity * text.fontSize / 100f;
+
+        while (percent < 1f)
+        {
+            percent += Time.deltaTime;
+            changeCharPercent += Time.deltaTime;
+
+            for (int i = 0; i < 2; i++)
+            {
+                CharInfoField charInfoField = charInfos[c[i]];
+
+                if (changeCharPercent > 0.3f)
+                {
+                    cached[charInfoField.charIndex] = (char)Random.Range(33, 126);
+                    text.SetText(cached);
+                }
+
+                //float size = 1 - percent;
+                for (int j = 0; j < 4; j++)
+                {
+                    Vector3 offset = new Vector3(
+                        Random.Range(-glitchIntensity, glitchIntensity),
+                        Random.Range(-glitchIntensity, glitchIntensity),
+                        0);
+                    charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j] + offset;
+
+                }
+
+                var meshInfo = text.textInfo.meshInfo[charInfoField.materialIndex];
+                meshInfo.mesh.vertices = meshInfo.vertices;
+                text.UpdateGeometry(meshInfo.mesh, charInfoField.materialIndex);
+
+            }
+
+            if (changeCharPercent > 0.3f) changeCharPercent = 0;
+
+
+
+            yield return null;
+        }
+
+
+        for (int i = 0; i < c.Length; i++)
+        {
+            CharInfoField charInfoField = charInfos[c[i]];
+            cached[charInfoField.charIndex] = charInfoField.orgChar;
+            text.SetText(cached);
+
+            for (int j = 0; j < 4; j++)
+                charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j];
+        }
+
+    }
+    #endregion
+}
+
+public class CharInfoField
+{
+
+    public TMP_TextInfo tmp;
+    public TMP_CharacterInfo charInfo;
+    public Vector3[] vertices;
+    public Vector3[] originalVertices;
+    public int vertexIndex;
+    public int materialIndex;
+    public Vector3 charCenter;
+
+    public char orgChar;
+    public int charIndex;
+
+    public CharInfoField(TMP_TextInfo tmp, TMP_CharacterInfo charInfo)
+    {
+        this.tmp = tmp;
+        this.charInfo = charInfo;
+        vertexIndex = charInfo.vertexIndex;
+        materialIndex = charInfo.materialReferenceIndex;
+
+        vertices = tmp.meshInfo[materialIndex].vertices;
+        originalVertices = (Vector3[])vertices.Clone();
+        charCenter = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2;
+
+        orgChar = charInfo.character;
+        charIndex = charInfo.index;
+
+    }
 }
--- Assets/Scripts/Test/TestTextEffect.cs
+++ Assets/Scripts/Test/TestTextEffect.cs
@@ -1,21 +1,16 @@
 
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class TestTextEffect : MonoBehaviour
 {
   [SerializeField] TextMeshProUGUI main_Text;
 
-    public AnimationCurve curve;
     CharInfoField[] charInfos;
-
-    float glitchIntensity = 5f; // 흔들리는 강도
-    float glitchFrequency = 0.1f; // 흔들림 업데이트 주기
-    int characterChangeCount = 4; // 문자가 변하는 횟수
-    float characterChangeInterval = 0.2f; // 문자 변환 간격
+    float glitchIntensity = 2f; // 흔들리는 강도
 
 
     private void Start(){
@@ -27,119 +22,113 @@ public class TestTextEffect : MonoBehaviour
             charInfos[i] = new CharInfoField(main_Text.textInfo,main_Text.textInfo.characterInfo[i]);
         }
 
-        // charInfo = main_Text.textInfo.characterInfo[1];
-        // vertexIndex = charInfo.vertexIndex;
-        // materialIndex = charInfo.materialReferenceIndex;
-        // vertices = main_Text.textInfo.meshInfo[materialIndex].vertices;
-        
-        // originalVertices = vertices.Clone() as Vector3[];
-        // charCenter = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2;
 
         StartCoroutine(EffectCo(charInfos));
     }
 
 
-    private void Update(){
-        
-        // OnEffect(charInfos);
-        
-    }
-
-    private void OnEffect(CharInfoField[] charInfos){
-
-        for(int i =0; i< charInfos.Length;i++){
-            CharInfoField charInfoField = charInfos[i];
-
-            for(int j = 0; j<4;j++){
-                Vector3 offset = new Vector3(
-                    Random.Range(-glitchIntensity, glitchIntensity),
-                    Random.Range(-glitchIntensity, glitchIntensity),
-                    0);
-                charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j] + offset;
-
-            }
-
-            var meshInfo = main_Text.textInfo.meshInfo[charInfoField.materialIndex];
-            meshInfo.mesh.vertices = meshInfo.vertices;
-            main_Text.UpdateGeometry(meshInfo.mesh,charInfoField.materialIndex);    
-
-        }
-        // for (int i = 0; i < main_Text.textInfo.meshInfo.Length; i++)
-        // {
-        //         var meshInfo = main_Text.textInfo.meshInfo[i];
-        //         meshInfo.mesh.vertices = meshInfo.vertices;
-        //         main_Text.UpdateGeometry(meshInfo.mesh,i);
-        // }
-            
-    }
-
 
     IEnumerator EffectCo(CharInfoField[] charInfos){
         while(true){
             int num = Random.Range(0,charInfos.Length);
-            yield return CharEffectCo(charInfos[num]);
-            // for(int i =0; i< charInfos.Length;i++){
-            //     CharInfoField charInfoField = charInfos[i];
-            //     if(charInfoField.charInfo.isVisible)
-            //     yield return CharEffectCo(charInfoField);
+            yield return CharEffectCo();
 
-            // }
-
-            
+            yield return new WaitForSeconds(1f);
         }
     }
 
-    float duration = 1;
-    IEnumerator CharEffectCo(CharInfoField charInfoField){
+    IEnumerator CharEffectCo(){
         float percent =0;
 
-        while(percent < .5f){
-            percent += Time.deltaTime;
+        char[] cached = main_Text.text.ToCharArray();
+        float changeCharPercent = 0;
 
-            for(int j = 0; j<4;j++){
-                Vector3 offset = new Vector3(
-                    Random.Range(-glitchIntensity, glitchIntensity),
-                    Random.Range(-glitchIntensity, glitchIntensity),
-                    0);
-                charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j] + offset;
+        int[] c= new int[2];
+        for (int i = 0; i < 2; i++) 
+        {
+            int ran = Random.Range(0, main_Text.text.Length);
+            c[i] = ran;
+        }
+
+        while (percent < 1f){
+            percent += Time.deltaTime;
+            changeCharPercent += Time.deltaTime;
+
+            for(int i = 0; i < 2; i++)
+            {
+                CharInfoField charInfoField = charInfos[c[i]];
+               
+                if (changeCharPercent > 0.3f)
+                {
+                    cached[charInfoField.charIndex] = (char)Random.Range(33, 126);
+                    main_Text.SetText(cached);
+                }
+
+                //float size = 1 - percent;
+                for (int j = 0; j < 4; j++)
+                {
+                    Vector3 offset = new Vector3(
+                        Random.Range(-glitchIntensity, glitchIntensity),
+                        Random.Range(-glitchIntensity, glitchIntensity),
+                        0);
+                    charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j] + offset;
+
+                }
+
+                var meshInfo = main_Text.textInfo.meshInfo[charInfoField.materialIndex];
+                meshInfo.mesh.vertices = meshInfo.vertices;
+                main_Text.UpdateGeometry(meshInfo.mesh, charInfoField.materialIndex);
 
             }
 
-            var meshInfo = main_Text.textInfo.meshInfo[charInfoField.materialIndex];
-            meshInfo.mesh.vertices = meshInfo.vertices;
-            main_Text.UpdateGeometry(meshInfo.mesh,charInfoField.materialIndex);  
-           
+            if (changeCharPercent > 0.3f) changeCharPercent = 0;
+
+
+
             yield return null;
         }
 
-        for(int j = 0; j<4;j++) 
-            charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j];
+
+        for(int i = 0; i < c.Length; i++)
+        {
+            CharInfoField charInfoField = charInfos[c[i]];
+            cached[charInfoField.charIndex] = charInfoField.orgChar;
+            main_Text.SetText(cached);
+
+            for (int j = 0; j < 4; j++)
+                charInfoField.vertices[charInfoField.vertexIndex + j] = charInfoField.originalVertices[charInfoField.vertexIndex + j];
+        }
 
     }
          
-
-        
 }
     
 
- public class CharInfoField{
+// public class CharInfoField{
 
-        public TMP_TextInfo tmp;
-        public TMP_CharacterInfo charInfo;
-        public Vector3[] vertices;
-        public Vector3[] originalVertices;
-        public int vertexIndex;
-        public int materialIndex;
-        public Vector3 charCenter;
-        public CharInfoField(TMP_TextInfo tmp,TMP_CharacterInfo charInfo){
-            this.tmp = tmp;
-            this.charInfo = charInfo;
-            vertexIndex = charInfo.vertexIndex;
-            materialIndex = charInfo.materialReferenceIndex;
+//        public TMP_TextInfo tmp;
+//        public TMP_CharacterInfo charInfo;
+//        public Vector3[] vertices;
+//        public Vector3[] originalVertices;
+//        public int vertexIndex;
+//        public int materialIndex;
+//        public Vector3 charCenter;
 
-            vertices = tmp.meshInfo[materialIndex].vertices;
-            originalVertices = (Vector3[])vertices.Clone();
-            charCenter = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2;
+//        public char orgChar;
+//        public int charIndex;
 
-        }
-}
+//        public CharInfoField(TMP_TextInfo tmp,TMP_CharacterInfo charInfo){
+//            this.tmp = tmp;
+//            this.charInfo = charInfo;
+//            vertexIndex = charInfo.vertexIndex;
+//            materialIndex = charInfo.materialReferenceIndex;
+
+//            vertices = tmp.meshInfo[materialIndex].vertices;
+//            originalVertices = (Vector3[])vertices.Clone();
+//            charCenter = (vertices[vertexIndex] + vertices[vertexIndex + 2]) / 2;
+
+//            orgChar = charInfo.character;
+//            charIndex = charInfo.index;
+
+//        }
+//}
