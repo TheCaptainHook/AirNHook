d2d7fb6c9e7c08a260abea2be552557256cba9f5
hygge31 <121877159+hygge31@users.noreply.github.com>
2025-01-23 21:09:31 +0900

    feat/ network

--- Assets/Scripts/MapObjects/DoorOpeningAnim.cs
+++ Assets/Scripts/MapObjects/DoorOpeningAnim.cs
@@ -45,14 +45,23 @@ public class DoorOpeningAnim : NetworkBehaviour
     [Command(requiresAuthority = false)]
     public void Enter(GameObject obj)
     {
+       RpcEnter(obj);
+    }
+    [ClientRpc]
+    private void RpcEnter(GameObject obj)
+    {
         Panel.Enter(obj);
     }
     [Command(requiresAuthority = false)]
     public void Exit(GameObject obj)
     {
+        RpcExit(obj);
+    }
+    [ClientRpc]
+    private void RpcExit(GameObject obj)
+    {
         Panel.Exit(obj);
     }
-
     //private void Update()
     //{
     //    //TEST  
