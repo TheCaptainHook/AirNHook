b83638c6bb32d790073bfa5fbc2d3f5117e5b0cc
hygge31 <121877159+hygge31@users.noreply.github.com>
2025-02-10 21:35:39 +0900

    feat/sync

--- Assets/Scripts/MapObjects/InteractableObject.cs
+++ Assets/Scripts/MapObjects/InteractableObject.cs
@@ -104,7 +104,7 @@ public class InteractableObject : NetworkBehaviour, IInteractable, IInhalable
         _isFixed = false;
         _isGrab = false;
         _canInteract = true;
-        ChangeState(false);z
+        ChangeState(false);
         ShowEButton();
 
         _rigidbody.bodyType = _originType;
