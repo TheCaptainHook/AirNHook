0444dc1a0ddd5d527cd9bd5bcd093365d711058b
hygge31 <121877159+hygge31@users.noreply.github.com>
2025-03-31 11:56:14 +0900

    test/

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/Portal/Portal_Net.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/Portal/Portal_Net.cs
@@ -65,7 +65,7 @@ public class Portal_Net : NetworkBehaviour
 
 #region  Sync Init
   
-    [Command]
+    [Command(requiresAuthority = false)]
     private void Cmd_SyncData()
     {
         Debug.Log("CMD 1");
@@ -93,7 +93,8 @@ public class Portal_Net : NetworkBehaviour
     public override void OnStartClient()
     {
         base.OnStartClient();
-        if (!onSync) StartCoroutine(Delay(()=> { Cmd_SyncData(); }));
+        //if (!onSync) StartCoroutine(Delay(()=> { }));
+        Cmd_SyncData();
 
     }
 
