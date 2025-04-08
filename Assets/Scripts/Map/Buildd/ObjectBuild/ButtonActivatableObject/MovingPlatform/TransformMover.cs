6c1af8a2fc214d66c8b1aa1360185fc284af3768
hygge31 <121877159+hygge31@users.noreply.github.com>
2025-04-08 21:46:21 +0900

    feat

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/MovingPlatform/TransformMover.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/MovingPlatform/TransformMover.cs
@@ -4,6 +4,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using System;
 using System.Collections;
+using DG.Tweening.Core.Easing;
 
 public class TransformMover : NetworkBehaviour
 {
@@ -64,82 +65,57 @@ public class TransformMover : NetworkBehaviour
     #endregion
 
     #region Set Transform Network
-    //[Command(requiresAuthority = false)]
-    //private void Cmd_SetTransform(uint mainID, uint platformID)
-    //{
-    //    Rpc_SetTransform(mainID, platformID);
-    //}
-
-    //[ClientRpc]
-    //private void Rpc_SetTransform(uint mainID, uint platformID )
-    //{
-    //    var main = NetworkClient.spawned.TryGetValue(mainID, out NetworkIdentity main_identity) ? main_identity : null;
-    //    var platform = NetworkClient.spawned.TryGetValue(platformID, out NetworkIdentity platform_identity) ? platform_identity : null;
-
-    //    var networkRd = main.TryGetComponent(out NetworkRigidbodyUnreliable2D netRb) ? netRb : null;
-
-    //    try
-    //    {
-    //        if (platform == null)
-    //        {
-    //            if(identity.isOwned && networkRd) networkRd.enabled = false;
-
-    //            main.transform.SetParent(parent,true);
-    //            movingPlatform = null;
-
-    //            if (identity.isOwned && networkRd) networkRd.enabled = true;
-
-    //        }
-    //        else
-    //        {
-    //            //parent = transform.parent;
-    //            movingPlatform = platform.gameObject.TryGetComponent(out MovingPlatform component) ? component : null;
-
-    //            if (identity.isOwned && networkRd) networkRd.enabled = false;
-    //            main.transform.SetParent(platform.transform,true);
-    //            if (identity.isOwned && networkRd) networkRd.enabled = true;
-
-
-    //        }
-    //    }
-    //    catch (Exception e)
-    //    {
-    //        Debug.Log(e);
-    //    }
-
-
-    //}
     [Command(requiresAuthority = false)]
-    public void Cmd_SetTransform(uint mainID, uint platformID)
+    private void Cmd_SetTransform(uint mainID, uint platformID)
     {
-        if (!NetworkServer.spawned.TryGetValue(mainID, out var main_identity)) return;
-        if (!NetworkServer.spawned.TryGetValue(platformID, out var platform_identity)) return;
-
-        // 서버에서 SetParent (transform 구조 반영)
-        var main = main_identity.transform;
-        var platform = platform_identity.transform;
-
-        Vector3 pos = main.position;
- 
-        main.SetParent(platform, true);
-        main.position = pos;
-
-        // ✅ 클라이언트에도 반영해줘야 함
-        Rpc_SetTransform(main_identity.netId, platform_identity.netId);
+        Rpc_SetTransform(mainID, platformID);
     }
+
     [ClientRpc]
     private void Rpc_SetTransform(uint mainID, uint platformID)
     {
-        if (!NetworkClient.spawned.TryGetValue(mainID, out var main_identity)) return;
-        if (!NetworkClient.spawned.TryGetValue(platformID, out var platform_identity)) return;
+        var main = NetworkClient.spawned.TryGetValue(mainID, out NetworkIdentity main_identity) ? main_identity : null;
+        var platform = NetworkClient.spawned.TryGetValue(platformID, out NetworkIdentity platform_identity) ? platform_identity : null;
 
-        var main = main_identity.transform;
-        var platform = platform_identity.transform;
+        var networkRd = main.TryGetComponent(out NetworkRigidbodyUnreliable2D netRb) ? netRb : null;
 
-        Vector3 pos = main.position;
- 
-        main.SetParent(platform, true);
-        main.position = pos;
+        try
+        {
+            if (platform == null)
+            {
+                //main.transform.SetParent(parent, true);
+                movingPlatform = null;
+            }
+            else
+            {
+                //parent = transform.parent;
+                movingPlatform = platform.gameObject.TryGetComponent(out MovingPlatform component) ? component : null;
+
+                //if (identity.isOwned && networkRd) networkRd.enabled = false;
+                //main.transform.SetParent(platform.transform, true);
+                //if (identity.isOwned && networkRd) networkRd.enabled = true;
+
+                if (identity.isOwned && networkRd)
+                {
+                    StartCoroutine(Delay(main.transform, platform.transform, networkRd));
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+
+
+    }
+
+
+    IEnumerator Delay(Transform main,Transform parent,NetworkRigidbodyUnreliable2D rb)
+    {
+        rb.enabled = false;
+        main.SetParent(parent);
+        yield return new WaitForSeconds(0.1f);
+        rb.enabled = true;
     }
     #endregion
 
