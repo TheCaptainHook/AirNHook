1373f62bff4036439902235ef75c96160e6e9458
hygge31 <121877159+hygge31@users.noreply.github.com>
2025-04-08 21:30:36 +0900

    test

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/MovingPlatform/TransformMover.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/MovingPlatform/TransformMover.cs
@@ -60,57 +60,87 @@ public class TransformMover : NetworkBehaviour
 
     }
     #region Recover
- 
+
     #endregion
 
     #region Set Transform Network
+    //[Command(requiresAuthority = false)]
+    //private void Cmd_SetTransform(uint mainID, uint platformID)
+    //{
+    //    Rpc_SetTransform(mainID, platformID);
+    //}
+
+    //[ClientRpc]
+    //private void Rpc_SetTransform(uint mainID, uint platformID )
+    //{
+    //    var main = NetworkClient.spawned.TryGetValue(mainID, out NetworkIdentity main_identity) ? main_identity : null;
+    //    var platform = NetworkClient.spawned.TryGetValue(platformID, out NetworkIdentity platform_identity) ? platform_identity : null;
+
+    //    var networkRd = main.TryGetComponent(out NetworkRigidbodyUnreliable2D netRb) ? netRb : null;
+
+    //    try
+    //    {
+    //        if (platform == null)
+    //        {
+    //            if(identity.isOwned && networkRd) networkRd.enabled = false;
+
+    //            main.transform.SetParent(parent,true);
+    //            movingPlatform = null;
+
+    //            if (identity.isOwned && networkRd) networkRd.enabled = true;
+
+    //        }
+    //        else
+    //        {
+    //            //parent = transform.parent;
+    //            movingPlatform = platform.gameObject.TryGetComponent(out MovingPlatform component) ? component : null;
+
+    //            if (identity.isOwned && networkRd) networkRd.enabled = false;
+    //            main.transform.SetParent(platform.transform,true);
+    //            if (identity.isOwned && networkRd) networkRd.enabled = true;
+
+
+    //        }
+    //    }
+    //    catch (Exception e)
+    //    {
+    //        Debug.Log(e);
+    //    }
+
+
+    //}
     [Command(requiresAuthority = false)]
-    private void Cmd_SetTransform(uint mainID, uint platformID)
+    public void Cmd_SetTransform(uint mainID, uint platformID)
     {
-        Rpc_SetTransform(mainID, platformID);
-    }
+        if (!NetworkServer.spawned.TryGetValue(mainID, out var main_identity)) return;
+        if (!NetworkServer.spawned.TryGetValue(platformID, out var platform_identity)) return;
 
-    [ClientRpc]
-    private void Rpc_SetTransform(uint mainID, uint platformID )
-    {
-        var main = NetworkClient.spawned.TryGetValue(mainID, out NetworkIdentity main_identity) ? main_identity : null;
-        var platform = NetworkClient.spawned.TryGetValue(platformID, out NetworkIdentity platform_identity) ? platform_identity : null;
+        // 서버에서 SetParent (transform 구조 반영)
+        var main = main_identity.transform;
+        var platform = platform_identity.transform;
 
-        var networkRd = main.TryGetComponent(out NetworkRigidbodyUnreliable2D netRb) ? netRb : null;
-
-        try
-        {
-            if (platform == null)
-            {
-                if(identity.isOwned && networkRd) networkRd.enabled = false;
-                
-                main.transform.SetParent(parent,true);
-                movingPlatform = null;
-
-                if (identity.isOwned && networkRd) networkRd.enabled = true;
-
-            }
-            else
-            {
-                //parent = transform.parent;
-                movingPlatform = platform.gameObject.TryGetComponent(out MovingPlatform component) ? component : null;
-
-                if (identity.isOwned && networkRd) networkRd.enabled = false;
-                main.transform.SetParent(platform.transform,true);
-                if (identity.isOwned && networkRd) networkRd.enabled = true;
-
-
-            }
-        }
-        catch (Exception e)
-        {
-            Debug.Log(e);
-        }
-
-
-    }
-
+        Vector3 pos = main.position;
  
+        main.SetParent(platform, true);
+        main.position = pos;
+
+        // ✅ 클라이언트에도 반영해줘야 함
+        Rpc_SetTransform(main_identity.netId, platform_identity.netId);
+    }
+    [ClientRpc]
+    private void Rpc_SetTransform(uint mainID, uint platformID)
+    {
+        if (!NetworkClient.spawned.TryGetValue(mainID, out var main_identity)) return;
+        if (!NetworkClient.spawned.TryGetValue(platformID, out var platform_identity)) return;
+
+        var main = main_identity.transform;
+        var platform = platform_identity.transform;
+
+        Vector3 pos = main.position;
+ 
+        main.SetParent(platform, true);
+        main.position = pos;
+    }
     #endregion
 
 
