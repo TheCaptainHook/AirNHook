899cd903d411278fb7a1f868e3c9ddbcec2d142a
hygge31 <yiuter1@hanmail.net>
2025-05-10 17:33:12 +0900

    feat

--- Assets/Scripts/UI/SelectMapUI_var3/UI_StageSelect_var3_Dummy.cs
+++ Assets/Scripts/UI/SelectMapUI_var3/UI_StageSelect_var3_Dummy.cs
@@ -173,50 +173,7 @@ public class UI_StageSelect_var3_Dummy: UI_Base
         computer.GetComponent<StageSelectorComputer>().Talking();
     }
     
-    //public IEnumerator ProcessInputWithDelay(int num)
-    //{
-    //    // inputProcessed = true;
-    //    switch (num)
-    //    {
-    //        case 1:
-    //            curSelectTextLineIndex++;
-    //            SelectTextLine();
-    //            break;
-    //        case 2:
-    //            curSelectTextLineIndex--;
-    //            SelectTextLine();
-    //            break;
-    //        case 3:
-    //            if (curSelectTextLine == null || !curSelectTextLine.onSelectable)
-    //                break;
 
-    //            switch (_PrograssLevel)
-    //            {
-    //                case PrograssLevel.One:
-    //                    Select_PrograssLevel_1();
-    //                    break;
-    //                case PrograssLevel.Two:
-    //                    Select_PrograssLevel_2();
-    //                    break;
-    //                case PrograssLevel.Three:
-    //                    if (curSelectTextLine.mainSentence == null) yield break;
-    //                    textLineList[pathTextLineIndex].WriteText($"/{curSelectTextLine.mainSentence}");
-    //                    _PrograssCoroutine = StartCoroutine(Select_PrograssLevel_3Co());
-    //                    break;
-    //            }
-    //            break;
-    //        case 4:
-    //            BackPrograss();
-    //            break;
-    //        case 5:
-    //            if(_PrograssLevel == PrograssLevel.Three)mapInfo_UI.Reset();
-    //            Shutdown();
-    //            break;
-    //    }
-
-    //    yield return new WaitForSeconds(inputDelay);
-    //    // inputProcessed = false;
-    //}
   //---------------------------------------------------------------------------Refectoring 0414
     private Coroutine queue_Input_Coroutine;
     private Queue<int> inputQueue;
@@ -230,10 +187,11 @@ public class UI_StageSelect_var3_Dummy: UI_Base
     }
     IEnumerator Queue_Input_Co()
     {
-        while(inputQueue.Count >0)
+        
+        while (inputQueue.Count >0)
         {
-             yield return new WaitUntil(()=> !onPrograss);
             // StartCoroutine(ProcessInputWithDelay(inputQueue.Dequeue()));
+            //yield return new WaitUntil(() => !onPrograss);
             switch (inputQueue.Dequeue())
             {
                 case 1:
