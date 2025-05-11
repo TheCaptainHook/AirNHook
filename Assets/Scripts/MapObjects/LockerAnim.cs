c20387c2f1a209aac4f4b11298011322f92ab899
pure-night <58593218+pure-night@users.noreply.github.com>
2025-05-11 19:19:55 +0900

    Feat: Locker anim

--- Assets/Scripts/MapObjects/LockerAnim.cs
+++ Assets/Scripts/MapObjects/LockerAnim.cs
@@ -1,11 +1,14 @@
 using System;
 using Mirror;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class LockerAnim : NetworkBehaviour, IInteractable
 {
     [SerializeField] private GameObject _disappearingObj;
     [SerializeField] private CharacterType _characterType;
+    [SerializeField] private SpriteRenderer _doorSpriteRenderer;
+    private GameObject _player;
     private ObjectTypeEnum _objectType = ObjectTypeEnum.Interaction;
     private Animator _animator;
     public Vector2 offset;
@@ -32,16 +35,6 @@ public class LockerAnim : NetworkBehaviour, IInteractable
     {
         OnChangingAnimation?.Invoke();
     }
-    
-    //이하 애니메이션 테스트용 코드
-    
-    private void Update()
-    {
-        if (IsChanging)
-        {
-            _animator.SetTrigger(Changing);
-        }
-    }
 
     public void DestroyGO()
     {
@@ -50,7 +43,22 @@ public class LockerAnim : NetworkBehaviour, IInteractable
 
     public void Interaction(Transform accessor)
     {
-        CmdChangeCharacter(accessor.root.gameObject);
+        _player = accessor.root.gameObject;
+
+        var playerSM = _player.GetComponent<PlayerSM>();
+        playerSM.canControl = false;
+
+        var playerRigidbody = _player.GetComponent<Rigidbody2D>();
+        playerRigidbody.gravityScale = 0f;
+        playerRigidbody.velocity = Vector3.zero;
+
+        playerSM.transform.position = transform.position + new Vector3(0, 1f);
+
+        var playerSortingGroup = _player.GetComponent<SortingGroup>();
+        playerSortingGroup.sortingOrder = 0;
+        CmdChangeSortingOrder(_player);
+
+        _animator.SetTrigger(Changing);
     }
 
     public bool CanInteract()
@@ -63,12 +71,36 @@ public class LockerAnim : NetworkBehaviour, IInteractable
         return;
     }
 
+    public void ChangeCharacter()
+    {
+        if (_player is not null && _player.GetComponent<NetworkIdentity>().isLocalPlayer)
+        {
+            CmdChangeCharacter(_player);
+            _animator.SetTrigger("Restock");
+        }
+
+        _player = null;
+    }
+
     [Command(requiresAuthority = false)]
     private void CmdChangeCharacter(GameObject player)
     {
         Managers.Network.ReplacePlayer(player.GetComponent<NetworkIdentity>().connectionToClient,
             _characterType,
             transform.position + new Vector3(0, 0.2f));
+    }
+
+    [Command(requiresAuthority = false)]
+    private void CmdChangeSortingOrder(GameObject player)
+    {
+        RpcChangeSortingOrder(player);
+    }
+
+    [ClientRpc(includeOwner = false)]
+    private void RpcChangeSortingOrder(GameObject player)
+    {
+        var playerSortingGroup = player.GetComponent<SortingGroup>();
+        playerSortingGroup.sortingOrder = 0;
     }
 
     public ObjectTypeEnum GetObjectType()
