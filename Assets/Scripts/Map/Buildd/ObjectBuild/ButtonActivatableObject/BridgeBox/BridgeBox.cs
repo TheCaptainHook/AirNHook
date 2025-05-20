5771db93b9939518ce53c71773156062cbdfaf81
hygge31 <yiuter1@hanmail.net>
2025-05-21 03:44:10 +0900

    feat/BridgeBOx

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/BridgeBox/BridgeBox.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/BridgeBox/BridgeBox.cs
@@ -30,126 +30,54 @@ using UnityEngine;
             }   
             return default(T);
         }
-        public override async void SetData<T>(T data)
+    public override async void SetData<T>(T data)
+    {
+        try
         {
-            try
+            if (typeof(T) == typeof(ButtonActivatableObjectStruct))
             {
-                if (typeof(T) == typeof(ButtonActivatableObjectStruct))
+                ButtonActivatableObjectStruct objData = (ButtonActivatableObjectStruct)(object)data;
+                ButtonActivatedObjectStruct = objData;
+
+                bridgeLength = objData.bridgeLength;
+                connectionPoint = objData.connectionPoint;
+
+                if (Application.isPlaying)
                 {
-                    ButtonActivatableObjectStruct objData = (ButtonActivatableObjectStruct)(object)data;
-                    ButtonActivatedObjectStruct = objData;
+                    //BridgeBox_Net.SetData(ButtonActivatedObjectStruct);
+                    BridgeBox_Net.Server_InitSync();
 
-                    bridgeLength = objData.bridgeLength;
-                    connectionPoint = objData.connectionPoint;
-
+                    await new Util().Delay(() => { CheckActiveRequirAmount(); });
+                }
             }
-            }
-            catch
-            {
-                Debug.Log($"ERROR,{typeof(T)}");
-            }
-
-        if (Application.isPlaying)
-        {
-            // BridgeSetting();
-            //BridgeBox_Net.Server_SetData(bridgeLength,connectionPoint,ButtonActivatedObjectStruct.position);
-            //BridgeBox_Net.Server_InitSync();
-            BridgeBox_Net.SetData(ButtonActivatedObjectStruct);
-            BridgeBox_Net.Server_InitSync();
-
-            await new Util().Delay(() => { CheckActiveRequirAmount(); });
         }
-    }
-        #endregion
+        catch
+        {
+            Debug.Log($"ERROR,{typeof(T)}");
+        }
 
-
-
-
-    // private void BridgeSetting(){
-
-    //     // CreateConnectionObject();
-    //     // SetBridgeCollider();
-    // }
-
-
-    #region  Main
-    protected override void Activation()
-    {
-        BridgeBox_Net.Cmd_SetOnActive();
-    }
-    protected override void Deactivated()
-    {
-        BridgeBox_Net.Cmd_SetOnActive();
 
     }
-    // private void ConnectBridge(){
-    //     DrawLine();
-    //     bridgeCol.enabled = true;
-    // }
-    // private void DrawLine(){
-    //     lineRenderer.positionCount =2;
-    //     lineRenderer.SetPosition(0,lineRenderer.transform.position);
-    //     lineRenderer.SetPosition(1,connectionPoint+GetOffset());
-    // }
-
-    //  private void DisconnectBridge(){
-    //     lineRenderer.positionCount = 0;
-    //     bridgeCol.enabled =false;
-    // }
-
-
-    // private void SetBridgeCollider(){
-    //     GameObject obj = new GameObject("bridge");
-    //     bridgeCol = obj.AddComponent<BoxCollider2D>();
-
-    //     Vector2 a = lineRenderer.gameObject.transform.position;
-    //     Vector2 b = connectionPoint + GetOffset();
-
-    //     Vector2 mid = (a+b)/2;
-    //     float distance = Vector2.Distance(a,b);
-    //     Vector2 dir = (b-a).normalized;
-
-    //     bridgeCol.size = new Vector2(distance,bridgeCol.size.y);
-    //     obj.transform.position = mid;
-    //     float angle = Mathf.Atan2(dir.y,dir.x) * Mathf.Rad2Deg;
-    //     obj.transform.rotation = Quaternion.Euler(0,0,angle);
-
-    //     obj.layer = LayerMask.NameToLayer("Ground/NotHookable");
-    //     obj.transform.SetParent(transform);
-
-    //     bridgeCol.enabled = false;
-    // }
-
-
-    // private Vector2 GetOffset(){
-    //     return transform.position - lineRenderer.transform.position;
-    // }
-
     #endregion
 
 
-    // private void CreateConnectionObject(){
-    //     GameObject obj = new GameObject("Connect Object");
 
-    //     GameObject spO = Instantiate(spriteObj);
-    //     spO.transform.localScale = new Vector3(-1,1,1);
-    //     spO.transform.SetParent(obj.transform);
 
-    //     Rigidbody2D rb = obj.AddComponent<Rigidbody2D>();
-    //     rb.isKinematic = true;
-    //     rb.gravityScale = 0;
+   
+    #region  Main
+    protected override void Activation()
+    {
+        BridgeBox_Net.Server_ChangeOnActive(true);
+    }
+    protected override void Deactivated()
+    {
+        BridgeBox_Net.Server_ChangeOnActive(false);
 
-    //     BoxCollider2D bcol = obj.AddComponent<BoxCollider2D>();
-    //     bcol.offset = this.boxCol.offset;
-    //     bcol.size  = this.boxCol.size;
+    }
 
-    //     obj.transform.rotation = transform.rotation;
-    //     obj.transform.position = connectionPoint;
 
-    //     obj.transform.SetParent(transform);
-    //     obj.layer  = transform.gameObject.layer;
+    #endregion
 
-    // }
 
     private Vector2 GetConnectionPoint(){
             Vector2 dir = transform.right;
--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/BridgeBox/BridgeBox_Net.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/BridgeBox/BridgeBox_Net.cs
@@ -17,7 +17,7 @@ public class BridgeBox_Net : NetworkBehaviour
     public Vector2 connectionPoint;
     public Vector2 position;
 
-    [SyncVar(hook = nameof(OnChangeActive))] public bool onActive;
+    [SyncVar]public bool onActive;
 
 
 
@@ -25,11 +25,10 @@ public class BridgeBox_Net : NetworkBehaviour
     private BoxCollider2D Collider => GetComponent<BoxCollider2D>();
 
 
-
-
     #region Init
 
     public bool onSync;
+
     [Server]
     public void Server_InitSync()
     {
@@ -57,85 +56,79 @@ public class BridgeBox_Net : NetworkBehaviour
         transform.rotation = data.quaternion;
 
         CreateBridge();
+
+        if (onActive) Active();
+        else Deactive();
+
         onSync = true;
     }
 
     public override void OnStartClient()
     {
         base.OnStartClient();
-        if (!onSync) StartCoroutine(Delay());
+        if (!onSync) Cmd_InitSync();
 
     }
 
-    IEnumerator Delay()
-    {
-       while(!NetworkClient.ready) yield return null;
-        Cmd_InitSync();
-    }
+    //#region Client Only
+    //[Server]
+    //public void Server_RequirCheck()
+    //{
+    //    if(Main.activeRequirAmount == Main.curActiveBtn)
+    //    {
+    //        Rpc_RequirCheck(true);
+    //    }
+    //    else
+    //    {
+    //        Rpc_RequirCheck(false);
+    //    }
+    //}
+    //[ClientRpc]
+    //public void Rpc_RequirCheck(bool onOff)
+    //{
+    //    if(!isServer && isClient)
+    //    {
+    //        if(onOff)
+    //        {
+    //            Active();
+    //        }
+    //        else
+    //        {
+    //            Deactive();
+    //        }
+    //    }
+    //}
+    //#endregion
+
 
     #endregion
 
 
-    
+
     [Server]
-    public void Server_ChangeOnActive()
+    public void Server_ChangeOnActive(bool onOff)
     {
-        onActive = !onActive;
-    }
-    private void OnChangeActive(bool old,bool newVal)
-    {
-        if (newVal) Active();
-        else Deactive();
+        onActive = onOff;
+       Rpc_ChangeOnActive(onOff);
     }
 
-   // [Server]
-   //public void Server_SetData(float bridgeLength,Vector2 connectionPoint,Vector2 position)
-   //{
-   //     this.bridgeLength = bridgeLength;
-   //     this.connectionPoint = connectionPoint;
-   //     this.position = position;
-
-   //     //Rpc_BridgeSetting();
-   //     //CreateConnectionObject();
-   //     //SetBridgeCollider();
-   // }
-
-    [Command(requiresAuthority = false)]
-    public void Cmd_SetOnActive()
+    [ClientRpc]
+    private void Rpc_ChangeOnActive(bool onOff)
     {
-        if(isServer)
-        Server_ChangeOnActive();
+        if(onOff)
+        {
+            Active();
+        }
+        else
+        {
+            Deactive();
+        }
     }
 
-    //[ClientRpc]
-    //private void Rpc_BridgeSetting() 
-    //{
-    //    CreateConnectionObject();
-    //    SetBridgeCollider();
-    //}
+
 
     #region  ---------------------------------------Server_Util
-    //private void OnChangeConnectionPoint(Vector2 old, Vector2 newVal)
-    //{
-    //    //CreateConnectionObject();
-    //    //SetBridgeCollider();
-    //    StartCoroutine(WaitforSync());
-    //}
 
-    //IEnumerator WaitforSync()
-    //{
-
-    //    while(bridgeLength <= 0 || connectionPoint == Vector2.zero || this.position == Vector2.zero)
-    //    {
-    //        Debug.Log("Sync Wait");
-    //        yield return null;
-    //    }
-        
-    //    transform.position = position;
-
-    //    CreateConnectionObject();
-    //    SetBridgeCollider();
-    //}
     public void CreateBridge()
     {
         CreateConnectionObject();
@@ -144,21 +137,12 @@ public class BridgeBox_Net : NetworkBehaviour
 
     private void CreateConnectionObject()
     {
-        //GameObject obj = new GameObject("Connect Object");
-        //obj.transform.SetParent(transform);
-
-        //obj.transform.position = transform.right * bridgeLength + transform.position;
-        //obj.transform.localRotation = Quaternion.Euler(0, 0, 0);
-
 
         GameObject spO = Instantiate(spriteObj);
         spO.name = "Connect Object";
         spO.transform.SetParent(transform);
         spO.transform.position = transform.right * bridgeLength + transform.position;
 
-        //spO.transform.position = transform.right * bridgeLength + transform.position;
-
-        //spO.transform.localPosition = Vector3.zero;
         spO.transform.localRotation = Quaternion.Euler(0, 0, 0);
         spO.transform.localScale = new Vector3(-1, 1, 1);
 
@@ -175,7 +159,6 @@ public class BridgeBox_Net : NetworkBehaviour
 
     private void SetBridgeCollider()
     {
-        // lineRenderer.SetPosition(0, lineRenderer.transform.position);
 
         Vector2 a = lineRenderer.gameObject.transform.position;
         Vector2 b = connectionPoint + GetOffset();
@@ -205,86 +188,19 @@ public class BridgeBox_Net : NetworkBehaviour
 
 
 
-
-
-    //[Command]
-    //public void Cmd_Activation()
-    //{
-    //    Rpc_Activation();
-    //}
-    //[ClientRpc]
-    //private void Rpc_Activation()
-    //{
-    //    bridgeCollider.enabled = true;
-    //    DrawLine();
-    //}
-
-
-    //[Command]
-    //public void Cmd_Deactivated()
-    //{
-    //    Rpc_Deactivated();
-    //}
-    //[ClientRpc]
-    //private void Rpc_Deactivated()
-    //{
-    //    lineRenderer.positionCount = 0;
-    //    bridgeCollider.enabled = false;
-    //}
-
     private void Active()
     {
-        // StartCoroutine(On());
+
         lineRenderer.enabled = true;
         bridgeCollider.enabled = true;
         //DrawLine();
     }
     private void Deactive()
     {
-        // StartCoroutine(Off());
-        //lineRenderer.positionCount = 0;
+
         lineRenderer.enabled = false;
         bridgeCollider.enabled = false;
     }
-
-    //------Test
-    // IEnumerator On()
-    // {
-    //     float percent = 0;
-    //     bridgeCollider.enabled = true;
-
-    //     Vector3 targetLine = connectionPoint + GetOffset();
-
-    //     while(percent <1)
-    //     {
-    //         percent += Time.fixedDeltaTime;
-    //         // bridgeCollider.transform.localScale = Vector3.Lerp(Vector3.zero,Vector3.one,percent);
-    //         Vector3 line = Vector3.Lerp(lineRenderer.transform.position,targetLine,percent);
-
-    //         lineRenderer.SetPosition(1,line);
-    //         yield return null;
-    //     }
-
-    //     // bridgeCollider.transform.localScale = Vector3.one;
-    //     lineRenderer.SetPosition(1,targetLine);
-    // }
-    // IEnumerator Off()
-    // {
-    //     float percent = 1;
-    //     Vector3 targetLine = connectionPoint + GetOffset();
-    //     while(percent>0)
-    //     {
-    //         percent -=Time.fixedDeltaTime;
-    //         // bridgeCollider.transform.localScale = Vector3.Lerp(Vector3.one,Vector3.zero,percent);
-    //         Vector3 line = Vector3.Lerp(lineRenderer.transform.position,targetLine,percent);
-    //         lineRenderer.SetPosition(1,line);
-
-    //         yield return null;
-    //     }
-    //     bridgeCollider.enabled = false;
-    //     // bridgeCollider.transform.localScale = Vector3.zero;
-    //     lineRenderer.SetPosition(1,lineRenderer.transform.position);
-    // }
 
 
      private void DrawLine()
