d074284fa14d00b5d54b447ddde724454368cc33
hygge31 <yiuter1@hanmail.net>
2025-05-23 21:44:47 +0900

    feat/SawObject TakeDamage

--- Assets/Scripts/Map/Buildd/ObjectBuild/SawObject.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/SawObject.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
 
 public class SawObject : BuildObj
@@ -32,18 +33,33 @@ public class SawObject : BuildObj
 
     }
 
-
+    public float addForcePower;
     private void OnTriggerEnter2D(Collider2D other)
     {
         // 충돌한 객체가 IDamageable 인터페이스를 가지고 있는지 확인
         if (other.TryGetComponent(out IDamageable damageable) && !turnOff)
         {
+            if (NetworkServer.active)
+            {
+                var rb = other.TryGetComponent(out Rigidbody2D _rb) ? _rb : null;
+                if (rb != null)
+                {
+                    rb.velocity = Vector2.zero;
+                    
+                    rb.AddForce(GetTargetDir(other) * addForcePower, ForceMode2D.Impulse);
+                }
+            }
+            
             // If successful, apply damage
             damageable.TakeDamage();
         }
     }
 
-
+    private Vector2 GetTargetDir(Collider2D target)
+    {
+        return (target.transform.position - transform.position).normalized;
+    
+    }
     //private void OnCollisionEnter2D(Collision2D other)
     //{
     //    if (other.gameObject.TryGetComponent(out IDamageable damageable))
