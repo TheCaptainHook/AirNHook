dc84963526f64a54b6c8b945f3d90df44427c603
hygge31 <yiuter1@hanmail.net>
2025-05-29 20:17:52 +0900

    feat/

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/MovingPlatform/TransformMover.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonActivatableObject/MovingPlatform/TransformMover.cs
@@ -1,4 +1,5 @@
 
+using DG.Tweening.Core.Easing;
 using Mirror;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -13,49 +14,93 @@ public class TransformMover : NetworkBehaviour
     public MovingPlatform movingPlatform;
 
     NetworkIdentity identity;
+    NetworkIdentity Identity { get { identity ??= GetComponent<NetworkIdentity>(); return identity; } }
+
+
 
     uint nullNetID = 99999;
 
-    #region Recover
 
-    #endregion
+    [SerializeField] LayerMask movingPlatformLayer;
+    [SerializeField] Vector3 layOffset;
+
+
+    public Collider2D col;
+
     private void Awake()
     {
-        identity = GetComponent<NetworkIdentity>();
+        movingPlatformLayer = 1 << 15;
+        col =GetComponent<Collider2D>();    
+
     }
+
+    RaycastHit2D hit;
+    Vector3 offset;
 
     private void FixedUpdate()
     {
-        if (movingPlatform && identity.isOwned)
-        {     
-            transform.position += (Vector3)movingPlatform.dir;
-        }
-    }
+        //if (movingPlatform && identity.isOwned)
+        //{     
+        //    transform.position += (Vector3)movingPlatform.dir;
+        //}
+        //Bounds bounds = GetComponent<Collider2D>().bounds;
 
+        Vector3 offset = new Vector3(0, col.bounds.extents.y, 0);
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.TryGetComponent(out MovingPlatform movingPlatform) && identity.isOwned)
+        hit = Physics2D.Raycast(transform.position - offset + layOffset, -Vector2.up, 0.1f, movingPlatformLayer);
+        Debug.DrawRay(transform.position -offset + layOffset,-Vector2.up * 0.1f, Color.green);
+        //Debug.DrawRay(hit);
+
+        if (hit.collider != null && hit.collider.TryGetComponent(out MovingPlatform component) && Identity.isOwned)
         {
-            var platformID = movingPlatform.TryGetComponent(out NetworkIdentity identity) ? identity.netId : nullNetID;
-            Cmd_SetTransform(platformID,transform.position);
-        }
-     
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        if (movingPlatform)
-        {
-            Cmd_SetTransform(nullNetID, transform.position);
+            transform.position += (Vector3)component.dir;
         }
 
+
+
+
+        //if (movingPlatform != null && Identity.isOwned)
+        //{
+        //    transform.position += (Vector3)movingPlatform.dir;
+        //}
+
     }
-    #region Recover
 
-    #endregion
+//#if UNITY_EDITOR
+//    private void OnDrawGizmos()
+//    {
+
+//        Bounds bounds = GetComponent<Collider2D>().bounds;
+
+//        Vector3 offset = new Vector3(0, transform.position.y + bounds.extents.y, 0);
+
+//        Gizmos.color = Color.red;
+//        Gizmos.DrawRay(transform.position - offset, -Vector3.up * 0.1f);
+
+//    }
+//#endif
 
 
- 
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.TryGetComponent(out MovingPlatform movingPlatform) && identity.isOwned)
+    //    {
+    //        var platformID = movingPlatform.TryGetComponent(out NetworkIdentity identity) ? identity.netId : nullNetID;
+    //        Cmd_SetTransform(platformID,transform.position);
+    //    }
+
+    //}
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (movingPlatform)
+    //    {
+    //        Cmd_SetTransform(nullNetID, transform.position);
+    //    }
+
+    //}
+
+
+
 
     #region Set Transform Network
     [Command(requiresAuthority = false)]
