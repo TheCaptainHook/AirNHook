d5011b65e5acb3c42163dcb37772317c2c40bf6e
hygge31 <yiuter1@hanmail.net>
2025-08-10 22:34:14 +0900

    feat/0810

--- Assets/Scripts/UI/UI_EndingCredits.cs
+++ Assets/Scripts/UI/UI_EndingCredits.cs
@@ -2,34 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 
-public class UI_EndingCradit : UI_Base
+public class UI_EndingCredits : UI_Base
 {
     [Header("Refs")]
     [SerializeField] RectTransform viewport;
     private float _defViewportY = -1080f;
-    private float _curviewPortY = 0;
+    private float _maxViewportY = 1600;
     [SerializeField] RectTransform content;
-    [SerializeField] TMP_Text creditText;
+
     [SerializeField] TMP_FontAsset _font;
     private float _defaultFontSize = 30;
     private float _boldFontSize = 35;
     private bool _setText = false;
 
     #region  Scroll
-    private bool _onScroll = false;
+    Coroutine _scrollingCoroutine = null;
     [Header("Scroll")]
     [SerializeField, Tooltip("기본 스크롤 속도 (px/sec)")]
     float baseSpeed = 60f;
-    [SerializeField, Tooltip("시작 대기 (초)")]
-    float startDelay = 1.0f;
-    [SerializeField, Tooltip("끝에서 대기 (초)")]
-    float endHold = 1.0f;
-    [SerializeField, Tooltip("끝자리 여백 (px)")]
-    float bottomPadding = 100f;
-    [SerializeField, Tooltip("Time.timeScale의 영향을 받지 않게 하려면 체크")]
-    bool useUnscaledTime = true;
+
     #endregion
     string testText =
     @"
@@ -50,6 +44,13 @@ Alex Kim
 /bMUSIC
 Kevin Brown
 
+/bAssets & Tools Used
+“Fantasy GUI Pack” by XYZ Studio (Unity Asset Store)
+“Pixel Adventure Sprite Pack” by ABC Artist (Itch.io)
+Background Music: “Epic Journey” by MusicMan (Licensed)
+Sound Effects from freesound.org (CC-BY 3.0)
+Font: “Nanum Gothic” by Naver Corporation (OFL License)
+
 /bSPECIAL THANKS
 All Our Players!
 
@@ -64,27 +65,21 @@ All Our Players!
     protected override void OpenUI()
     {
         base.OpenUI();
-        if (!_setText) SplitText(testText);
-
-        _onScroll = true;
+        _option.Disable();
+        _scrollingCoroutine = StartCoroutine(ScrollingCo());
+       
     }
     protected override void CloseUI()
     {
+        StopCoroutine(_scrollingCoroutine);
+        _scrollingCoroutine = null;
+        content.gameObject.SetActive(false);
+        content.anchoredPosition = new Vector2(0, _defViewportY);
+
         base.CloseUI();
-        Reset();
+        
     }
 
-    private void Reset()
-    {
-        _onScroll = false;
-        _curviewPortY = _defViewportY;
-        viewport.anchoredPosition = new Vector2(viewport.anchoredPosition.x, _curviewPortY);
-    }
-
-    protected override void Start()
-    {
-        SplitText(testText);
-    }
     private void SplitText(string text)
     {
         string[] lines = text.Split(new[] { '\n' }, System.StringSplitOptions.None);
@@ -93,6 +88,7 @@ All Our Players!
             TextMeshProUGUI textMesh = new GameObject("CreditLine", typeof(TextMeshProUGUI)).GetComponent<TextMeshProUGUI>();
             textMesh.transform.SetParent(content, false);
             textMesh.font = _font;
+            textMesh.color = Color.black;
             if (line.StartsWith("/b"))
             {
                 textMesh.fontStyle = FontStyles.Bold;
@@ -110,5 +106,35 @@ All Our Players!
         _setText = true;
     }
 
-    
+    InputAction _option => Managers.Game.playerInput.uiActions.Option;
+
+    private float fastMultiplier = 4f;
+    private IEnumerator ScrollingCo()
+    {
+        if (!_setText) SplitText(testText);
+        yield return new WaitForSeconds(0.1f);
+        content.anchoredPosition = new Vector2(0, _defViewportY);
+        content.gameObject.SetActive(true);
+
+        while (content.anchoredPosition.y < _maxViewportY)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                break;
+            }
+        
+            float mult = Input.GetKey(KeyCode.Space) ? fastMultiplier : 1f;
+            float speed = baseSpeed * Time.deltaTime * mult;
+
+            content.anchoredPosition = new Vector2(0, content.anchoredPosition.y + speed);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+
+        _option.Enable();
+        CloseUI();
+       
+    }
+
+
 }
