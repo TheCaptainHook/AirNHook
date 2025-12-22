b357e0ad0de53bdf1b3e8df46ff1e8ae9fcdc222
문정현 <58593218+pure-night@users.noreply.github.com>
2025-12-22 18:13:51 +0900

    Fix:InputSystem

--- Assets/Scripts/Character/Player/PlayerStateMachine.cs
+++ Assets/Scripts/Character/Player/PlayerStateMachine.cs
@@ -56,7 +56,11 @@ public class PlayerStateMachine : StateMachine
     public override void SubscribeInput()
     {
         input.playerActions.Move.started += MoveStarted;
+        input.playerActions.Move.performed += MoveStarted;
+        input.playerActions.Move.canceled += MoveStarted;
         input.playerActions.VerticalMove.started += VerticalMoveStarted;
+        input.playerActions.VerticalMove.performed += VerticalMoveStarted;
+        input.playerActions.VerticalMove.canceled += VerticalMoveStarted;
         input.playerActions.Jump.started += JumpStarted;
         input.playerActions.Jump.performed += JumpPerformed;
         input.playerActions.Jump.canceled += JumpCanceled;
@@ -65,7 +69,11 @@ public class PlayerStateMachine : StateMachine
     public override void UnsubscribeInput()
     {
         input.playerActions.Move.started -= MoveStarted;
+        input.playerActions.Move.performed -= MoveStarted;
+        input.playerActions.Move.canceled -= MoveStarted;
         input.playerActions.VerticalMove.started -= VerticalMoveStarted;
+        input.playerActions.VerticalMove.performed -= VerticalMoveStarted;
+        input.playerActions.VerticalMove.canceled -= VerticalMoveStarted;
         input.playerActions.Jump.started -= JumpStarted;
         input.playerActions.Jump.performed -= JumpPerformed;
         input.playerActions.Jump.canceled -= JumpCanceled;
