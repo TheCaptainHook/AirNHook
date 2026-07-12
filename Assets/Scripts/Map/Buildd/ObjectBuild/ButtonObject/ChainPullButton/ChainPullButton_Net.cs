d486ec358af9513e9297e38affca6efcacdafdd3
hygge31 <yiuter1@hanmail.net>
2026-07-12 16:16:42 +0900

    f

--- Assets/Scripts/Map/Buildd/ObjectBuild/ButtonObject/ChainPullButton/ChainPullButton_Net.cs
+++ Assets/Scripts/Map/Buildd/ObjectBuild/ButtonObject/ChainPullButton/ChainPullButton_Net.cs
@@ -442,7 +442,7 @@ public class ChainPullButton_Net : ButtonEntity_Net
                     }
                 }else if(sm.characterType == CharacterType.Hook)
                 {
-                   _s_r_cur_chain_length = Vector2.Distance(_r_Chain._start.position, sm.grabSource.sourceTransform.position);
+                   _s_r_cur_chain_length = Vector2.Distance(_r_Chain._start.position, sm.transform.position);
                 }
                 
             }else
